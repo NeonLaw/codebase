0e0f149b3f771c9f450718e5ec2918c4fa222cd2
Nicholas Shook <nick@neonlaw.com>
2020-05-07 15:16:01 -0700

    Fix Accessibility Spec
    Buttons need an aria-label - this button fixes the issue by combining
    the a and the Button tag to the same element.

--- src/components/socialMediaIcons.tsx
+++ src/components/socialMediaIcons.tsx
@@ -1,60 +1,59 @@
-import { Box, Button } from '@chakra-ui/core';
+/* eslint-disable */
+// @ts-nocheck
+/* eslint-enable */
 import {
   FaFacebookF, FaGithub, FaLinkedinIn, FaMailchimp, FaTwitter
 } from 'react-icons/fa';
+import { Box } from '@chakra-ui/core';
+import { Button } from '@components/button';
 import React from 'react';
 
 export const SocialMediaIcons = ({ display }) => (
   <Box display={display} mb="7px">
-    <a
+    <Button
+      as="a"
       href="https://www.twitter.com/neonlaw"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="@Neon Law on Twitter"
     >
-      <Button>
-        <FaTwitter style={{ display: 'inline' }} />
-      </Button>
-    </a>
-    <a
+      <FaTwitter style={{ display: 'inline' }} />
+    </Button>
+    <Button
+      as="a"
       href="https://www.facebook.com/neonlaw"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Neon Law Facebook page"
     >
-      <Button>
-        <FaFacebookF style={{ display: 'inline' }} />
-      </Button>
-    </a>
-    <a
+      <FaFacebookF style={{ display: 'inline' }} />
+    </Button>
+    <Button
+      as="a"
       href="https://www.linkedin.com/company/neon-law"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Neon Law LinkedIn page"
     >
-      <Button>
-        <FaLinkedinIn style={{ display: 'inline' }} />
-      </Button>
-    </a>
-    <a
+      <FaLinkedinIn style={{ display: 'inline' }} />
+    </Button>
+    <Button
+      as="a"
       href="https://github.com/neonlaw"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Neon Law GitHub page"
     >
-      <Button>
-        <FaGithub style={{ display: 'inline' }} />
-      </Button>
-    </a>
-    <a
+      <FaGithub style={{ display: 'inline' }} />
+    </Button>
+    <Button
+      as="a"
       href="https://mailchi.mp/f364242f585f/neonlaw"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Neon Law Monthly - an Email Newsletter"
     >
-      <Button>
-        <FaMailchimp style={{ display: 'inline' }} />
-      </Button>
-    </a>
-  </Box>
+      <FaMailchimp style={{ display: 'inline' }} />
+    </Button>
+  </Box >
 );
