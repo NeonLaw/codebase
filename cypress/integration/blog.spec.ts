ec4f29bf5a8bf6d41ad091e68b069bf47b4fc677
Nicholas Shook <nick@neonlaw.com>
2020-06-21 16:45:37 -0700

    fix spelling mistake

--- cypress/integration/blog.spec.ts
+++ cypress/integration/blog.spec.ts
@@ -2,7 +2,7 @@
 
 describe('CIDR blocks blog page', () => {
   it('has an edit button that loads the edit page on GitHub', () => {
-    cy.visit('/blog/cir-blocks');
+    cy.visit('/blog/cidr-blocks');
     cy.get('[data-cy="edit-on-github"]').should('be.visible');
   });
 });
