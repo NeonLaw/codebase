d971ef0094104399e44eccc95de81407c6f0d06c
Nicholas Shook <nick@neonlaw.com>
2020-06-27 15:04:27 -0700

    optionallly set tab spec

--- cypress/integration/keyboardAccessibility.spec.ts
+++ cypress/integration/keyboardAccessibility.spec.ts
@@ -2,11 +2,14 @@
 /// <reference types="cypress" />
 
 describe('Keyboard Navigation Tests', () => {
-  it('body have user-is-tabbing class only if the user is tabbing', () => {
-    localStorage.setItem('darkMode', 'true');
-    cy.visit('/');
-    cy.get('body').not('user-is-tabbing');
-    cy.get('a').tab();
-    cy.get('body').should('have.class', 'user-is-tabbing');
-  });
+  it(
+    'body have user-is-tabbing class only if the user is tabbing',
+    { browser: '!chrome' },
+    () => {
+      localStorage.setItem('darkMode', 'true');
+      cy.visit('/');
+      cy.get('body').not('user-is-tabbing');
+      cy.get('a').tab();
+      cy.get('body').should('have.class', 'user-is-tabbing');
+    });
 });
