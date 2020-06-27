de2b4790026b3362dd2d122306ea9768ce41a932
Nicholas Shook <nick@neonlaw.com>
2020-06-27 15:02:12 -0700

    try setting localstorage darkMode to true

--- cypress/integration/keyboardAccessibility.spec.ts
+++ cypress/integration/keyboardAccessibility.spec.ts
@@ -3,6 +3,7 @@
 
 describe('Keyboard Navigation Tests', () => {
   it('body have user-is-tabbing class only if the user is tabbing', () => {
+    localStorage.setItem('darkMode', 'true');
     cy.visit('/');
     cy.get('body').not('user-is-tabbing');
     cy.get('a').tab();
