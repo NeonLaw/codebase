ad9118fa6473bdab1ba0cf8ae65a965b3f1487d0
nick shook <nick@shook.family>
2020-08-08 14:21:49 -0700

    wait after in portal

--- packages/interface/cypress/integration/portal.spec.ts
+++ packages/interface/cypress/integration/portal.spec.ts
@@ -17,6 +17,7 @@ describe('Visiting /portal', () => {
       cy.loginAsPortalUser().then(() => {
         cy.visit('/portal');
         cy.contains('Profile').click();
+        cy.wait(1000);
         cy.reload();
         cy.url().should('include', '/portal/profile');
         cy.get('[data-testid="portal-profile-form-name"]').
