b4fb6c304e9f8a21a06de6b0427cdca67f85b72b
nick shook <nick@shook.family>
2020-08-08 14:44:15 -0700

    another wait statement

--- packages/interface/cypress/integration/portal.spec.ts
+++ packages/interface/cypress/integration/portal.spec.ts
@@ -19,13 +19,12 @@ describe('Visiting /portal', () => {
         cy.contains('Profile').click();
         cy.wait(1000);
         cy.reload();
+        cy.wait(1000);
         cy.url().should('include', '/portal/profile');
         cy.get('[data-testid="portal-profile-form-name"]').
           invoke('val').
           should('not.contain', name);
         cy.get('[data-testid="portal-profile-form-name"]').type(name);
-
-        cy.wait(2000);
 
         cy.get('[data-testid="portal-profile-form-submit"]').click();
 
