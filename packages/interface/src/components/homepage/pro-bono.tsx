e9217d62950063cc3944e28e5d761dedfd5b5902
nick shook <nick@shook.family>
2020-08-09 21:35:23 -0700

    Update pro-bono.tsx

--- packages/interface/src/components/homepage/pro-bono.tsx
+++ packages/interface/src/components/homepage/pro-bono.tsx
@@ -14,9 +14,9 @@ export const ProBono = () => {
         {intl.formatMessage({ id: 'pro_bono.title' })}
       </h2>
       <Box as="p" maxWidth={sizes.textContainerSmall}>
-        Finally, and most importantly, we are proudest of our commitment to{' '}
-        <UnderlineLink to="/pro-bono/">pro bono</UnderlineLink> work and wish{' '}
-        we could do it full time. 
+        Finally, and most importantly, we are proudest of our commitment to&nbsp;
+        <UnderlineLink to="/pro-bono/">pro bono</UnderlineLink> work and&nbsp;
+        wish we could do it full time.&nbsp;
         If you require assistance and have attained a{' '}
         {/*eslint-disable */}
         <UnderlineLink href="http://www.lacsnpro_bono.org/wp-content/uploads/2019/12/SOLA-Process-Service-FAQ-2020.pdf">
