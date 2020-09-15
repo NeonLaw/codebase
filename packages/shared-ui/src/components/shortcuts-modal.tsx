b4ac7d2823591518696c2a96ec12688ccce56d4c
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-09-15 10:19:36 +0500

    make the shortcuts modal tabable.

--- packages/shared-ui/src/components/shortcuts-modal.tsx
+++ packages/shared-ui/src/components/shortcuts-modal.tsx
@@ -33,6 +33,10 @@ const StyledShortCut = styled.li`
     border-radius: 5px;
     box-shadow: ${shadows.light2};
   }
+
+  body.user-is-tabbing &:focus {
+    outline: var(--outline);
+  }
 `;
 
 interface Shortcut {
@@ -85,6 +89,7 @@ const ShortcutsModal = () => {
                   <StyledShortCut
                     key={key}
                     style={{ borderColor: colors.borders[colorMode] }}
+                    tabIndex={0}
                   >
                     <div className="desc">{description}</div>
                     <div
