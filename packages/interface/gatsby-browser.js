f2cd9a9482cc8bf2fceb6674c40fbdfa210c5e03
nick shook <nick@shook.family>
2020-10-08 10:48:47 -0700

    fix linting

--- packages/interface/gatsby-browser.js
+++ packages/interface/gatsby-browser.js
@@ -1,6 +1,6 @@
 import { BaseLayout } from '@neonlaw/shared-ui/src/layouts/base';
-import React from 'react';
 import LogRocket from 'logrocket';
+import React from 'react';
 
 export const wrapPageElement = ({ element, props }) => {
   return <BaseLayout {...props}>{element}</BaseLayout>;
