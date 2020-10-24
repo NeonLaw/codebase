6209d2e4766d4c23d41e69c4f8f0943b4b3ceedd
nick shook <nick@shook.family>
2020-10-24 15:55:34 -0700

    Add better CI testing suite using docker compose
    This better mimics our cloud environments using Docker

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -5,6 +5,6 @@ while ! nc -z api 3000; do sleep 1; done;
 
 cp -vr ./packages/shared-ui/fonts ./packages/$PACKAGE_NAME/static
 
-yarn install --silent --ignore-optional
+yarn install --silent --ignore-optional --prefer-offline
 
 yarn workspace @neonlaw/$PACKAGE_NAME start
