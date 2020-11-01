470e6144fb6d40e1a94c0a7920174bee6f7f313b
nick shook <nick@shook.family>
2020-11-01 08:11:49 -0800

    change interface to install before waiting

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-while ! nc -z api 3000; do sleep 1; done;
-
 yarn install \
   --silent \
   --ignore-optional \
   --prefer-offline \
   --cache-folder ./node_modules
+
+while ! nc -z api 3000; do sleep 1; done;
 
 yarn workspace @neonlaw/$PACKAGE_NAME start
