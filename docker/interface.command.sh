1ea014e86d110b256f604c48055223b9203afb73
nick shook <nick@shook.family>
2020-11-01 09:57:05 -0800

    try building and serving instead of dev with filesystem notifications

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -7,6 +7,8 @@ yarn install \
   --prefer-offline \
   --cache-folder ./node_modules
 
+yarn workspace @neonlaw/$PACKAGE_NAME build
+
 while ! nc -z api 3000; do sleep 1; done;
 
 yarn workspace @neonlaw/$PACKAGE_NAME start
