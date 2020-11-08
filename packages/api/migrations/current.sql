745abfd386730e19724ad2fb003d510a1d74953f
nick shook <nick@shook.family>
2020-11-07 22:46:55 -0800

    added lookup for id

--- packages/api/migrations/current.sql
+++ packages/api/migrations/current.sql
@@ -1,1 +1,11 @@
 -- Enter migration here
+
+CREATE OR REPLACE FUNCTION find_or_create_letter_by_lob_identifier(lob_letters_json json) RETURNS void AS $$
+DECLARE
+  letter JSON;
+BEGIN
+  FOR letter IN (SELECT json_array_elements($1) element) LOOP
+    RAISE NOTICE '%', letter->>'id';
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql;
