ad8da867e9b7036b6dd9185796aebdcbc9b8bf40
nick shook <nick@shook.family>
2020-11-07 23:25:46 -0800

    add lob syncing

--- packages/api/migrations/current.sql
+++ packages/api/migrations/current.sql
@@ -4,8 +4,28 @@ CREATE OR REPLACE FUNCTION find_or_create_letter_by_lob_identifier(lob_letters_j
 DECLARE
   letter JSON;
 BEGIN
+  CREATE TEMPORARY TABLE temp_letters (
+    lob_identifier varchar,
+    lob_record json
+   );
+
   FOR letter IN (SELECT json_array_elements($1) element) LOOP
-    RAISE NOTICE '%', letter->>'id';
+    INSERT INTO temp_letters (lob_identifier, lob_record)
+    VALUES (letter->>'id', letter);
   END LOOP;
+
+  INSERT INTO letter (lob_identifier, lob_record)
+  SELECT lob_identifier, lob_record FROM temp_letters
+  ON CONFLICT (lob_identifier) DO UPDATE SET lob_record = EXCLUDED.lob_record;
+
+  INSERT INTO sync (resource_name) VALUES ('lob_letter');
+
+  DROP TABLE temp_letters;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE UNIQUE INDEX IF NOT EXISTS lob_identifier ON letter (lob_identifier);
+
+ALTER TABLE letter ALTER COLUMN addressor_id DROP NOT NULL;
+ALTER TABLE letter ALTER COLUMN addressee_id DROP NOT NULL;
+ALTER TABLE letter ALTER COLUMN document_id DROP NOT NULL;
