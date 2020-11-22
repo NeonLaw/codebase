c16e01e503a98478f4e5e4ac391207ec23dab992
nick shook <nick@shook.family>
2020-11-21 23:44:44 -0800

    reset dockerfile

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-export CYPRESS_SITE_URL="http://$PACKAGE_NAME:8000"
+yarn workspace @neonlaw/$PACKAGE_NAME build
 
 while ! nc -z api 3000; do sleep 1; done;
 
-yarn workspace @neonlaw/$PACKAGE_NAME dev
+yarn workspace @neonlaw/$PACKAGE_NAME start
