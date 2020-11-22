0a2fd89eb0e9f408354cad2f38ae8bd563659d57
nick shook <nick@shook.family>
2020-11-21 19:06:14 -0800

    dev instead of start to use dev variables

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -3,8 +3,6 @@ set -e
 
 export CYPRESS_SITE_URL="http://$PACKAGE_NAME:8000"
 
-yarn workspace @neonlaw/$PACKAGE_NAME build
-
 while ! nc -z api 3000; do sleep 1; done;
 
-yarn workspace @neonlaw/$PACKAGE_NAME start
+yarn workspace @neonlaw/$PACKAGE_NAME dev
