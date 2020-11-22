b4f7a433a2ac6403dc63e263426a0a46a27588c6
nick shook <nick@shook.family>
2020-11-22 09:03:07 -0800

    all on one line

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-export GATSBY_ACTIVE_ENV="development"
-
-yarn workspace @neonlaw/$PACKAGE_NAME build
+GATSBY_ACTIVE_ENV="development" yarn workspace @neonlaw/$PACKAGE_NAME build
 
 while ! nc -z api 3000; do sleep 1; done;
 
