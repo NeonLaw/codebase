47836c09010382dc56e2e59fec1be35b6d676f5c
nick shook <nick@shook.family>
2020-11-22 09:54:54 -0800

    no need to wait before starting

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -5,6 +5,4 @@ set -e
 export GATSBY_ACTIVE_ENV="development"
 yarn workspace @neonlaw/$PACKAGE_NAME build
 
-while ! nc -z api 3000; do sleep 1; done;
-
 yarn workspace @neonlaw/$PACKAGE_NAME start
