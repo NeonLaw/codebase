9730341a32373d179ba88b9772946f5e35c3e5ec
nick shook <nick@shook.family>
2020-11-21 17:08:47 -0800

    Update the ACCESSIBLE_BUTTONS flag on the portal.
    This also re-adds the Cypress portal spec which was previously failing.

---
+++ packages/interface/cypress/integration/portal.spec.ts
@@ -0,0 +1,41 @@
+/// <references types="Cypress" />
+/* eslint-disable no-undef */
+
+import faker from 'faker';
+
+describe('Visiting /portal', () => {
+  context('as an anonymous user', () => {
+    it('redirects the user back to the home screen', () => {
+      cy.visit('/portal');
+      cy.url().should('not.include', '/portal');
+    });
+  });
+  context('logged in as a portal user', () => {
+    it('renders the /portal pages and the user can change ther name', () => {
+      const name = faker.name.findName();
+
+      cy.loginAsPortalUser().then(() => {
+        cy.visit('/portal');
+        cy.contains('Profile').click();
+        cy.url().should('include', '/portal/profile');
+
+        cy.get('[data-testid="open-update-profile-modal"]').click();
+
+        cy.get('[data-testid="update-person-form-name"]').
+          invoke('val').
+          should('not.contain', name);
+        cy.get('[data-testid="update-person-form-name"]').type(name);
+
+        cy.get('[data-testid="update-person-form-accessible-buttons"]').click();
+
+        cy.get('[data-testid="update-person-form-submit"]').click();
+
+        cy.get('[data-testid="update-person-form-name"]').
+          invoke('val').
+          should('not.contain', name);
+        cy.get('[data-testid="portal-profile-card-name"]').
+          should('contain', name);
+      });
+    });
+  });
+});
