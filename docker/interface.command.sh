553943685635db5f75707efdd27ff004407a3a6c
nick shook <nick@shook.family>
2020-11-23 06:33:21 -0800

    try with api_url

--- docker/interface.command.sh
+++ docker/interface.command.sh
@@ -6,6 +6,7 @@ while ! nc -z api 3000; do sleep 1; done;
 
 # Bulid the app with development/testing environment variables
 export GATSBY_ACTIVE_ENV="development"
+export GATSBY_API_URL="http://api:3000/api/graphql"
 yarn workspace @neonlaw/$PACKAGE_NAME build
 
 yarn workspace @neonlaw/$PACKAGE_NAME start
