cb53f8829fa41e59e6a2c8ae20323c615f9a6650
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-11-26 20:24:13 +0500

    recreate delete you data landing page.

---
+++ packages/delete-your-data/src/components/nav/links.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    label: 'FAQs',
+    route: '#faqs'
+  },
+  {
+    label: 'About',
+    route: '/about'
+  },
+  {
+    label: 'Blog',
+    route: '/blog'
+  },
+  {
+    label: 'Contact',
+    route: '/contact'
+  }
+];
