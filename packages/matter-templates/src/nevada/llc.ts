f4572534dbb19bb5f798fc679667f59d294fd0b6
nick shook <nick@shook.family>
2020-11-29 11:17:08 -0800

    add llc test

--- packages/matter-templates/src/nevada/llc.ts
+++ packages/matter-templates/src/nevada/llc.ts
@@ -1,30 +1,8 @@
 import { Base } from '../base';
 
-export class LLC extends Base {
+export class NevadaLLC extends Base {
   documents = [
     'operating-agreement',
     'operating-agreement-revision'
   ];
-
-  createOperatingAgreementRevision(uploadFilename: string) {
-    const documentTemplate = {
-      id: 'rar',
-      name: 'nevada-llc-operating-agreement'
-    };
-
-    const fileExtension = '.docx';
-    const documentableTableName = 'matter_document';
-    const documentTemplateId = documentTemplate.id;
-    const filename = `/matters/${matterId}/operating-agreement`;
-
-    try {
-      // gcp copy file to private bucket
-
-      this.databaseSession().query(`
-    INSERT INTO document (file_extension, filename, documentable_table_name) ON CONFLICT target action
-  `);
-    } catch {
-
-    }
-  }
 }
