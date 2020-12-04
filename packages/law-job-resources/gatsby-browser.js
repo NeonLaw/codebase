909e634052e2ab2bea62b2f170986b96f1d29f1a
nick shook <nick@shook.family>
2020-12-03 21:06:06 -0800

    fix layouts with themes

--- packages/law-job-resources/gatsby-browser.js
+++ packages/law-job-resources/gatsby-browser.js
@@ -1,6 +1,6 @@
 import { BaseLayout } from '@neonlaw/shared-ui/src/layouts/baseLayout';
 import React from 'react';
-import { theme } from '@neonlaw/shared-ui/src/layouts/neonLaw';
+import { theme } from '@neonlaw/shared-ui/src/themes/neonLaw';
 
 export const wrapPageElement = ({ element, props }) => {
   return <BaseLayout theme={theme} {...props}>{element}</BaseLayout>;
