9ee29af8ed3c0d795aac123cfdc77ded1be3c836
nick shook <nick@shook.family>
2020-12-13 10:03:38 -0800

    add description

--- infrastructure/modules/application_user/main.tf
+++ infrastructure/modules/application_user/main.tf
@@ -1,6 +1,7 @@
 resource "google_service_account" "application_user_account" {
   account_id   = "application-user"
   display_name = "Application User"
+  description = "GCP Credentials used in Kubernetes apps"
 }
 
 resource "google_service_account_key" "application_user_accunt_key" {
