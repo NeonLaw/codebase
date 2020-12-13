accf1087f01c76c724bf7a21656e8bc82e8cae12
nick shook <nick@shook.family>
2020-12-12 22:37:30 -0800

    reword docker ignore

--- packages/server/migrations/current.sql
+++ packages/server/migrations/current.sql
@@ -1,1 +1,11 @@
 -- Enter migration here
+
+GRANT anonymous TO neon_law_admin;
+
+GRANT portal TO neon_law_admin;
+
+GRANT lawyer TO neon_law_admin;
+
+GRANT admin TO neon_law_admin;
+
+DROP TABLE IF EXISTS sync;
