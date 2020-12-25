b2bfc6751cfd2c832831f81ef913daa6f3113ee4
nick shook <nick@shook.family>
2020-12-24 20:13:46 -0800

    DRY update modals

--- packages/interface/src/components/modals/updateMatterTemplateModal.tsx
+++ packages/interface/src/components/modals/updateMatterTemplateModal.tsx
@@ -1,19 +1,9 @@
-import {
-  Modal,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  theme,
-  useColorMode,
-} from '@chakra-ui/core';
 import React from 'react';
 import { UpdateModalFormBuilder } from '../forms/updateModalFormBuilder';
-import { colors } from '../../themes/neonLaw';
 
 interface UpdateMatterTemplateModalProps {
-  isOpen: boolean;
   onClose(): void;
+  isOpen: boolean;
   id?: string;
 }
 
@@ -22,51 +12,33 @@ export const UpdateMatterTemplateModal = ({
   isOpen,
   onClose,
 }: UpdateMatterTemplateModalProps) => {
-  const { colorMode } = useColorMode();
-
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="xl">
-      <ModalOverlay>
-        <ModalContent
-          data-testid="update-matter-template-modal"
-          margin="8em 2em 0 2em"
-        >
-          <ModalHeader
-            fontWeight="normal"
-            fontSize={theme.fontSizes['xl0']}
-            color={colors.text[colorMode]}
-          >
-            Update Matter Template
-          </ModalHeader>
-          <ModalCloseButton style={{ color: colors.text[colorMode] }} />
-          <UpdateModalFormBuilder
-            resourceName="matterTemplate"
-            onClose={onClose}
-            currentValues={{ id }}
-            fields={[
-              {
-                name: 'name',
-                type: 'string',
-              },
-              {
-                name: 'javascriptModule',
-                type: 'codeEditor',
-              },
-              {
-                name: 'category',
-                options: [
-                  { label: 'Data Deletion', value: 'data-deletion' },
-                  { label: 'Inmate Letter', value: 'inmate-letter' },
-                  { label: 'Litigation', value: 'litigation' },
-                  { label: 'Estate', value: 'estate' },
-                  { label: 'Business', value: 'business' },
-                ],
-                type: 'select',
-              },
-            ]}
-          />
-        </ModalContent>
-      </ModalOverlay>
-    </Modal>
+    <UpdateModalFormBuilder
+      resourceName="matterTemplate"
+      onClose={onClose}
+      isOpen={isOpen}
+      currentValues={{ id }}
+      fields={[
+        {
+          name: 'name',
+          type: 'string',
+        },
+        {
+          name: 'javascriptModule',
+          type: 'codeEditor',
+        },
+        {
+          name: 'category',
+          options: [
+            { label: 'Data Deletion', value: 'data-deletion' },
+            { label: 'Inmate Letter', value: 'inmate-letter' },
+            { label: 'Litigation', value: 'litigation' },
+            { label: 'Estate', value: 'estate' },
+            { label: 'Business', value: 'business' },
+          ],
+          type: 'select',
+        },
+      ]}
+    />
   );
 };
