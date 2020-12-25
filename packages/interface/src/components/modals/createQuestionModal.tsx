71311834a0dced8b45bb8cacff1a07c62ac917ee
nick shook <nick@shook.family>
2020-12-25 15:02:27 -0800

    add question fields

--- packages/interface/src/components/modals/createQuestionModal.tsx
+++ packages/interface/src/components/modals/createQuestionModal.tsx
@@ -1,5 +1,6 @@
 import { CreateModalFormBuilder } from '../forms/createModalFormBuilder';
 import React from 'react';
+import { questionFields } from '../fields/questionFields';
 
 export const CreateQuestionModal = ({ isOpen, onClose }) => {
   return (
@@ -7,25 +8,7 @@ export const CreateQuestionModal = ({ isOpen, onClose }) => {
       resourceName="question"
       isOpen={isOpen}
       onClose={onClose}
-      fields={[
-        {
-          name: 'prompt',
-          required: true,
-          type: 'string',
-        },
-        {
-          name: 'questionType',
-          options: [
-            { label: 'Single Choice', value: 'single-choice' },
-            { label: 'Single Date', value: 'single-date' },
-            {
-              label: 'Single File Upload',
-              value: 'single-file-upload'
-            },
-          ],
-          type: 'select',
-        }
-      ]}
+      fields={questionFields}
     />
   );
 };
--- packages/interface/src/components/modals/updateQuestionModal.tsx
+++ packages/interface/src/components/modals/updateQuestionModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UpdateModalFormBuilder } from '../forms/updateModalFormBuilder';
+import { questionFields } from '../fields/questionFields';
 
 export const UpdateQuestionModal = ({ isOpen, onClose, question }) => {
   return (
@@ -8,25 +9,7 @@ export const UpdateQuestionModal = ({ isOpen, onClose, question }) => {
       currentValues={question}
       isOpen={isOpen}
       onClose={onClose}
-      fields={[
-        {
-          name: 'prompt',
-          required: true,
-          type: 'string',
-        },
-        {
-          name: 'questionType',
-          options: [
-            { label: 'Single Choice', value: 'single-choice' },
-            { label: 'Single Date', value: 'single-date' },
-            {
-              label: 'Single File Upload',
-              value: 'single-file-upload'
-            },
-          ],
-          type: 'select',
-        }
-      ]}
+      fields={questionFields}
     />
   );
 };
