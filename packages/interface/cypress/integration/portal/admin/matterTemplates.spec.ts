4ada644bc221a07f149aae9c1dc0e4a5acde706c
nick shook <nick@shook.family>
2020-12-26 19:30:32 -0800

    simpler matter templates spec

--- packages/interface/cypress/integration/portal/admin/matterTemplates.spec.ts
+++ packages/interface/cypress/integration/portal/admin/matterTemplates.spec.ts
@@ -9,38 +9,6 @@ describe('CRUDding Matter Templates', () => {
 
       cy.createMatterTemplate(matterTemplateName);
 
-      cy.loginAsAdminUser().then(() => {
-        cy.visit('/portal/admin/matter-templates');
-
-        cy.wait(1000);
-
-        cy.contains('Create Matter Template').click();
-
-        cy.wait(1000);
-
-        cy.get('[data-testid="create-matter-template-form"]')
-          .should('exist');
-
-        cy
-          .get('[data-testid="matter-template-form-name"]')
-          .type(matterTemplateName);
-
-        cy.get(
-          '[data-testid="matter-template-form-javascript-module"]'
-        ).type(faker.lorem.sentence());
-
-        cy
-          .get('[data-testid="create-matter-template-form-submit"]')
-          .click();
-
-        cy.get('[data-testid="create-matter-template-form"]')
-          .should('not.exist');
-
-        cy.get('[data-testid="create-matter-template-form"]')
-          .should('not.exist');
-
-      });
-
       cy.deleteMatterTemplate(matterTemplateName);
     }
   );
