e7b745881593e721a97ee48ff30cb80163f34dbc
nick shook <nick@shook.family>
2021-01-07 16:49:38 -0800

    Add create person from auth0 (#1431)

--- packages/server/tests/express/createPersonFromAuth0.test.ts
+++ packages/server/tests/express/createPersonFromAuth0.test.ts
@@ -1,13 +1,22 @@
+import * as faker from 'faker';
 import { app } from '../../src/app';
 import { expect } from '@jest/globals';
 import request from 'supertest';
 
-describe('English Locale Endpoint', () => {
-  it('renders english locales in JSON format', async () => {
+describe('Create a person from Auth0', () => {
+  it('returns the created user', async () => {
+    const email = `${faker.random.uuid()}@sink.sendgrid.com`;
+    const sub = faker.random.uuid();
+
     const res = await request(app)
       .post('/api/auth0-create-person')
-      .send();
+      .set('Content-Type', 'application/json')
+      .send({ email, sub });
 
     expect(res.status).toEqual(201);
+
+    expect(res.body).toHaveProperty('id');
+    expect(res.body).toHaveProperty('email');
+    expect(res.body).toHaveProperty('sub');
   });
 });
