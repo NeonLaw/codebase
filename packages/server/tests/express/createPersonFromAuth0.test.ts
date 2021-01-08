64de03698773999c203eeb96ee83622dbfee734d
nick shook <nick@shook.family>
2021-01-07 19:46:17 -0800

    add authorization header (#1434)

--- packages/server/tests/express/createPersonFromAuth0.test.ts
+++ packages/server/tests/express/createPersonFromAuth0.test.ts
@@ -3,14 +3,16 @@ import { app } from '../../src/app';
 import { expect } from '@jest/globals';
 import request from 'supertest';
 
-describe('Create a person from Auth0', () => {
+describe('Create a person from Auth0 with authentication token', () => {
   it('returns the created user', async () => {
     const email = `${faker.random.uuid()}@sink.sendgrid.com`;
     const sub = faker.random.uuid();
+    const authenticationToken = process.env.AUTH0_CLIENT_SECRET;
 
     const res = await request(app)
       .post('/api/auth0-create-person')
       .set('Content-Type', 'application/json')
+      .set('Authorization', `basic ${authenticationToken}`)
       .send({ email, sub });
 
     expect(res.status).toEqual(201);
