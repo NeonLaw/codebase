82a0cad3fd50496f5ad74ae0856601bbb1011376
nick shook <nick@shook.family>
2021-01-12 08:20:27 -0800

    try non-backup neo4j chart *NO PR*

--- infrastructure/kubernetes/modules/neo4j_helm/main.tf
+++ infrastructure/kubernetes/modules/neo4j_helm/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "neo4j" {
   name   = "${var.environment}-neo4j"
-  chart  = "https://github.com/neo4j-contrib/neo4j-helm/releases/download/4.2.0-1/neo4j-backup-4.2.0-1.tgz"
+  chart  = "https://github.com/neo4j-contrib/neo4j-helm/releases/download/4.2.0-1/neo4j-4.2.0-1.tgz"
   verify = false
 
   set {
