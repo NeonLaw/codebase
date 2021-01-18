2586a8dec195def13771e5c57610284a0e69c05c
nick shook <nick@shook.family>
2021-01-17 18:37:39 -0800

    Simpler port forwarding for Neo4j
    With port forwarding, we can access neo4j's browser on our local machine
    using GKE credentials.

--- infrastructure/modules/kubernetes_ingress/main.tf
+++ infrastructure/modules/kubernetes_ingress/main.tf
@@ -29,20 +29,6 @@ resource "kubernetes_ingress" "primary" {
     }
 
     rule {
-      host = var.environment == "production" ? "www.neonlaw.com" : "www.neonlaw.net"
-      http {
-        path {
-          backend {
-            service_name = "${var.environment}-neo4j-neo4j"
-            service_port = 7474
-          }
-
-          path = "/browser/*"
-        }
-      }
-    }
-
-    rule {
       host = var.environment == "production" ? "www.lawjobresources.com" : "www.lawjobresources.info"
       http {
         path {
