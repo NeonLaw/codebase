634675408c55c9c20749a8fa07b695d0088b729e
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-01-20 00:22:41 +0500

    adjust the height of the cards.
    fixes #1479

--- packages/interface/src/components/desktopHalfMobileFullCard.tsx
+++ packages/interface/src/components/desktopHalfMobileFullCard.tsx
@@ -13,18 +13,20 @@ export const DesktopHalfMobileFullCard = ({ children, to }) => {
   return (
     <Box
       flex={['0 0 100%', '0 0 100%', '0 0 100%', '0 48%']}
+      maxWidth="600px"
       cursor={to ? 'pointer' : null}
       as={to ? Link : null}
       to={to ? to : null}
       className="outline-bordered"
+      margin={['1em 0', '1em 0', '2em 0']}
     >
       <Box
         borderWidth="1px"
         overflow="hidden"
-        margin={['1em 0', '1em 0', '2em 0']}
         background={colors.background[colorMode]}
         borderColor={colors.borders[colorMode]}
         boxShadow={shadows.light2}
+        height="100%"
       >
         {children}
       </Box>
