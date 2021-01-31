9224e2e9cacf86ce6aa147dc8f1a1f611f4f92a9
nick shook <nick@shook.family>
2021-01-31 10:56:25 -0800

    wedding link

--- packages/interface/src/pages/public-trusts/shook-family/index.tsx
+++ packages/interface/src/pages/public-trusts/shook-family/index.tsx
@@ -1,9 +1,10 @@
 import { Breadcrumbs } from '../../../components/breadcrumbs';
+import { Link } from '../../../components/link';
 import { PublicTrustLayout } from '../../../layouts/publicTrustLayout';
 import React from 'react';
 import { Seo } from '../../../components/seo';
 
-const HomePage = () => {
+const ShookFamilyIndexPage = () => {
   const title = 'Shook Family Trust';
   const description = 'The Shook Family Trust managed by Neon Law.';
 
@@ -12,9 +13,12 @@ const HomePage = () => {
       <Breadcrumbs />
       <Seo title={title} description={description} />
       Shook Family
+      <Link to="/public-trusts/shook-family/wedding">
+        Shook Family Wedding, February 1, 2021
+      </Link>
     </PublicTrustLayout>
   );
 };
 
 /* eslint-disable-next-line import/no-default-export */
-export default HomePage;
+export default ShookFamilyIndexPage;
