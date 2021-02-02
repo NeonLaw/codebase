05052dd660e998d75387c05a9768d0534afddc3a
nick shook <nick@shook.family>
2021-02-02 15:37:47 -0800

    add findOrCreate logic to auth0 callback (#1592)
    * update findOrCreate Auth0

--- packages/server/src/createPerson.ts
+++ packages/server/src/createPerson.ts
@@ -20,6 +20,32 @@ export const createPerson = async (
 
   const { sub, email } = args;
 
+  const currentPersonQuery = await client.query(
+    'SELECT id, role FROM person WHERE sub = $1 LIMIT 1',
+    [sub]
+  );
+  const currentPerson = currentPersonQuery.rows[0];
+
+  if (currentPerson) {
+    await client.end();
+    return currentPerson;
+  }
+
+  const currentPersonByEmailQuery = await client.query(
+    'SELECT id, role FROM person WHERE email = $1 LIMIT 1',
+    [email]
+  );
+  const currentPersonByEmail = currentPersonByEmailQuery.rows[0];
+
+  if (currentPersonByEmail) {
+    await client.query(
+      'UPDATE person SET sub = $1 WHERE email = $2',
+      [sub, email]
+    );
+    await client.end();
+    return currentPersonByEmail;
+  }
+
   const personQuery = await client.query(
     'INSERT INTO person (sub, email) ' +
     'VALUES ($1, $2) RETURNING id, email, sub, created_at',
