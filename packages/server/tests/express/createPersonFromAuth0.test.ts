cc222c4e5f19d7d58d14a9efdc7bed4f624147c9
nick shook <nick@shook.family>
2021-02-02 15:19:18 -0800

    update findOrCreate Auth0

--- packages/server/tests/express/createPersonFromAuth0.test.ts
+++ packages/server/tests/express/createPersonFromAuth0.test.ts
@@ -4,7 +4,7 @@ import { expect } from '@jest/globals';
 import request from 'supertest';
 
 describe('Create a person from Auth0 with authentication token', () => {
-  it('returns the created user', async () => {
+  it('returns the created user or the current user', async () => {
     const email = `${faker.random.uuid()}@sink.sendgrid.com`;
     const sub = faker.random.uuid();
     const authenticationToken = process.env.AUTH0_CLIENT_SECRET;
@@ -20,5 +20,17 @@ describe('Create a person from Auth0 with authentication token', () => {
     expect(res.body).toHaveProperty('id');
     expect(res.body).toHaveProperty('email');
     expect(res.body).toHaveProperty('sub');
+
+    const res2 = await request(app)
+      .post('/api/auth0-create-person')
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `basic ${authenticationToken}`)
+      .send({ email, sub });
+
+    expect(res2.status).toEqual(201);
+
+    expect(res2.body).toHaveProperty('id');
+    expect(res2.body).toHaveProperty('email');
+    expect(res2.body).toHaveProperty('sub');
   });
 });
