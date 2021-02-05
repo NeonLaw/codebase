cdd9073d5556e62dd14c4ab83aae8f192acc8ef4
nick shook <nick@shook.family>
2021-02-04 21:45:35 -0800

    add question detail view

--- packages/interface/src/components/detailViews/questionDetailView.tsx
+++ packages/interface/src/components/detailViews/questionDetailView.tsx
@@ -1,23 +1,88 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Text, useColorMode } from '@chakra-ui/core';
+import { Button } from '../button';
+import ReactDiffViewer from 'react-diff-viewer';
 import { Skeleton } from '@chakra-ui/core';
+import { Textarea } from '../inputs';
 import { convertSlateToPlaintext } from '../../utils/slate';
+import { gutters } from '../../themes/neonLaw';
+import { useForm } from 'react-hook-form';
 import { useQuestionByIdQuery } from '../../utils/api';
 
 export const QuestionDetailView = ({ id }) => {
+  const [showAnswer, toggleShowAnswer] = useState(false);
+  const [userAnswer, changeUserAnswer] = useState(null);
+  const formRef = useRef<HTMLFormElement>(null);
+  const { control, handleSubmit, errors } = useForm();
   const { data, loading } = useQuestionByIdQuery(
     { variables: { id }}
   );
+  const { colorMode } = useColorMode();
 
+  const onSubmit = async ({ answer }) => {
+    changeUserAnswer(answer);
+    toggleShowAnswer(true);
+  };
 
   if (loading)  {
     return <Skeleton height="20px" />;
   }
 
   if (data) {
-    return (
+    const answer = convertSlateToPlaintext(data.questionById?.helpText);
+    const prompt  = data.questionById?.prompt;
+
+    return !showAnswer ? (
+      <form
+        onSubmit={handleSubmit(onSubmit as any)}
+        ref={formRef}
+      >
+        <Text fontSize="1.2em" marginBottom="1em">
+          {prompt}
+        </Text>
+        <Textarea
+          testId="flashcard-textarea"
+          control={control}
+          label={null}
+          name="answer"
+          placeholder="Type in the answer"
+          defaultValue={userAnswer}
+          errors={errors}
+        />
+        <Button
+          data-testid="flashcard-form-submit"
+          flash={false}
+          type="submit"
+          containerStyles={{marginTop: gutters.xSmallOne}}
+        >
+            Show Answer
+        </Button>
+      </form>
+    ) : (
       <>
-        <h2>{data.questionById?.prompt}</h2>
-        <p>{convertSlateToPlaintext(data.questionById?.helpText)}</p>
+        <Text fontSize="1.2em" marginBottom="1em">
+          {prompt}
+        </Text>
+        {answer === userAnswer ? (
+          <Text>You got it!</Text>
+        ) : (
+          <ReactDiffViewer
+            oldValue={answer}
+            newValue={convertSlateToPlaintext(userAnswer) || ''}
+            hideLineNumbers={true}
+            showDiffOnly={false}
+            splitView={false}
+            useDarkTheme={colorMode === 'dark'}
+          />
+        )}
+        <Button
+          flash={false}
+          containerStyles={{marginTop: gutters.xSmallOne}}
+          className="show-prompt"
+          onClick={() => { toggleShowAnswer(false); }}
+        >
+            Try Typing the Answer
+        </Button>
       </>
     );
   }
