7a6f297bc5873826c961c8032d8432f796b2ed31
nick shook <nick@shook.family>
2021-02-04 20:24:32 -0800

    add last value to select and SelectWithQuery components (#1619)

--- packages/server/src/utils/seeds.sql
+++ packages/server/src/utils/seeds.sql
@@ -4,4 +4,6 @@ INSERT INTO matter_template (name, javascript_module, category) VALUES ('Califor
 
 INSERT INTO questionnaire (name, matter_template_id) VALUES ('Community Property', (SELECT id FROM matter_template WHERE name = 'California Bar Prep' LIMIT 1)) ON CONFLICT DO NOTHING;
 
-INSERT INTO question (prompt, question_type, help_text) VALUES ('Character Evidence', 'text', '{"text": "Character evidence is generally inadmissible"}') ON CONFLICT DO NOTHING;
+INSERT INTO question (prompt, question_type, help_text)
+VALUES ('Character Evidence', 'text', '[{ "type": "paragraph", "children": [{ "text": "help" }]}]')
+ON CONFLICT DO NOTHING;
