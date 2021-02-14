26d0339bdcf8bbcc88d23eaf3cf6ea03a992ca63
nick shook <nick@shook.family>
2021-02-13 19:46:13 -0800

    move cypress tests to new web ui

--- docker/development.interface.command.sh
+++ docker/development.interface.command.sh
@@ -4,8 +4,4 @@ set -e
 # Wait until the API is launched before building and serving the web app
 while ! nc -z api 3000; do sleep 1; done;
 
-# Bulid the app with development/testing environment variables
-export GATSBY_ACTIVE_ENV="development"
-yarn workspace @neonlaw/$PACKAGE_NAME build
-
-yarn workspace @neonlaw/$PACKAGE_NAME start
+yarn workspace @neonlaw/web dev -p 8000
