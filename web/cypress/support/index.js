4653587aaf33cbd6ba1af4efc3371b4d625db4be
nick shook <nick@shook.family>
2021-02-14 21:54:43 -0800

    remove cypress-gatsby

--- web/cypress/support/index.js
+++ web/cypress/support/index.js
@@ -2,5 +2,4 @@ import './commands';
 import './matterTemplateCommands.ts';
 import 'cypress-axe';
 import '@testing-library/cypress/add-commands';
-import 'gatsby-cypress';
 import 'cypress-plugin-tab';
