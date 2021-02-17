da9d7cd1c8fb3572b42be67d6a53881e17a36d0b
nick shook <nick@shook.family>
2021-02-17 09:25:38 -0800

    add basic bar-prep page

---
+++ web/src/components/layouts/mdxLayout.tsx
@@ -0,0 +1,79 @@
+import { Box, Divider, Flex, Heading, useColorMode } from '@chakra-ui/react';
+import React, { ReactChildren } from 'react';
+import { colors, gutters } from '../../styles/neonLaw';
+import { ApolloProvider } from '@apollo/client';
+import { BaseFooter } from '../footer/baseFooter';
+import { Breadcrumbs } from '../breadcrumbs';
+import { Container } from '../container';
+import { EditOnGithub } from '../editOnGithub';
+import { Image } from '../image';
+import { MDXComponents } from '../mdxComponents';
+import { MDXProvider } from '@mdx-js/react';
+import { PublicNavigationBar } from '../navigationBars/public';
+import { Seo } from '../seo';
+import { ShareButtons } from '../shareButtons';
+import { getApolloClient } from '../../utils/getApolloClient';
+import { useAuth0 } from '@auth0/auth0-react';
+
+export const MdxLayout: React.FC<{
+  chlidren: ReactChildren;
+  meta: any;
+}> = ({ children, meta }) => {
+  const { title, slug, featuredImage, description } = meta;
+  const { colorMode } = useColorMode();
+  const { getAccessTokenSilently } = useAuth0();
+  const widescreen = 'false';
+  const apolloClient = getApolloClient(getAccessTokenSilently);
+
+  return (
+    <Flex minHeight="100vh" direction="column">
+      <Seo title={title} image={featuredImage} description={description} />
+      <ApolloProvider client={apolloClient}>
+        <>
+          <PublicNavigationBar />
+          <Box
+            background={colors.lighterBg[colorMode]}
+            color={colors.text[colorMode]}
+          >
+            <Box
+              as="main"
+              aria-label="Main Content"
+              flex={1}
+              padding="9rem 0 4rem"
+            >
+              <Container>
+                <Breadcrumbs />
+                <Heading
+                  as="h1"
+                  fontSize="xl"
+                  marginBottom={gutters.xSmall}
+                  fontWeight="400"
+                >
+                  {title}
+                </Heading>
+                {featuredImage && (
+                  <Box borderWidth="1px" rounded="lg" overflow="hidden">
+                    <Image
+                      src={featuredImage}
+                      alt={title}
+                      aspectRatio={widescreen ? 2 : 16 / 9}
+                    />
+                  </Box>
+                )}
+                <MDXProvider components={MDXComponents}>
+                  {children}
+                </MDXProvider>
+                <Divider margin="1em 0" />
+                <Flex width="100%" justifyContent="space-between">
+                  <ShareButtons slug={slug} siteUrl="https://www.neonlaw.com" />
+                  <EditOnGithub app="interface" path={slug} />
+                </Flex>
+              </Container>
+            </Box>
+          </Box>
+        </>
+      </ApolloProvider>
+      <BaseFooter isWhite={true} />
+    </Flex>
+  );
+};
