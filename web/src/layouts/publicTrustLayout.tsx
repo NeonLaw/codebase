0869a22393d36a670f585c5a584fec4b6f53a9b7
nick shook <nick@shook.family>
2021-02-24 11:40:40 -0800

    add basic shook family website

---
+++ web/src/layouts/publicTrustLayout.tsx
@@ -0,0 +1,26 @@
+import { Box, useColorMode } from '@chakra-ui/react';
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import { TrustFooter } from '../components/footer/trustFooter';
+import { colors } from '../styles/neonLaw';
+import { getApolloClient } from '../utils/getApolloClient';
+
+export const PublicTrustLayout = ({ children }) => {
+  const { colorMode } = useColorMode();
+  const apolloClient = getApolloClient();
+
+  return (
+    <>
+      <Box
+        bg={colors.background[colorMode]}
+        color={colors.text[colorMode]}
+        paddingBottom="2em"
+      >
+        <ApolloProvider client={apolloClient}>
+          {children}
+        </ApolloProvider>
+      </Box>
+      <TrustFooter />
+    </>
+  );
+};
