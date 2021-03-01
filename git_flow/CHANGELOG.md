cc069f227f0f4c5468f9114eb934c61d6c54c8fa
nick shook <nick@shook.family>
2021-02-28 18:45:19 -0800

    add very simple validate-branch function (#1776)
    * add very simple validate-branch function
    
    This function will check if a branch name begins with:
    
    * feature/
    * improvement/
    * bugfix/
    * hotfix/
    * hotfix-base/
    * release/
    * deployment/
    
    This corresponds to our git-flow and how branches should be added in.

--- git_flow/CHANGELOG.md
+++ git_flow/CHANGELOG.md
@@ -1,8 +1,14 @@
 # CHANGELOG
 
-## v0.0.2 (next)
+## v0.0.3 (next)
+
+- Add the `validate-branch` command which validates a passed in argument to see
+  if it begins with a proper prefix (#1776)
+
+## v0.0.2
 
 - Publish to crates.io from GitHub Actions
+
 ## v0.0.1
 
 - Publish to crates.io
--- git_flow/src/main.rs
+++ git_flow/src/main.rs
@@ -3,6 +3,7 @@
 // This example demonstrates clap's full 'custom derive' style of creating arguments which is the
 // simplest method of use, but sacrifices some flexibility.
 use clap::Clap;
+use regex::Regex;
 
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
@@ -10,10 +11,8 @@ use clap::Clap;
 #[clap(version = "0.1", author = "Support <support@neonlaw.com>")]
 struct Opts {
     /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short, long, default_value = "default.conf")]
+    #[clap(short, long, default_value = "~/.git_flow.conf")]
     config: String,
-    /// Some input. Because this isn't an Option<T> it's required to be used
-    input: String,
     /// A level of verbosity, and can be used multiple times
     #[clap(short, long, parse(from_occurrences))]
     verbose: i32,
@@ -23,7 +22,7 @@ struct Opts {
 
 #[derive(Clap)]
 enum SubCommand {
-    #[clap(version = "1.3", author = "Someone E. <someone_else@other.com>")]
+    ValidateBranch(ValidateBranch),
     Test(Test),
 }
 
@@ -32,15 +31,21 @@ enum SubCommand {
 struct Test {
     /// Print debug info
     #[clap(short)]
-    debug: bool
+    debug: bool,
+}
+
+/// Validate a branch name
+#[derive(Clap)]
+struct ValidateBranch {
+    /// Branch
+    branch_name: String,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    // Gets a value for config if supplied by user, or defaults to "git_flow.conf"
     println!("Value for config: {}", opts.config);
-    println!("Using input file: {}", opts.input);
 
     // Vary the output based on how many times the user used the "verbose" flag
     // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
@@ -61,7 +66,11 @@ fn main() {
                 println!("Printing normally...");
             }
         }
+        SubCommand::ValidateBranch(t) => {
+            let branch_name: String = t.branch_name;
+            println!("Validating Branch {}", branch_name);
+            let re = Regex::new(r"^(feature|improvement)/.*$").unwrap();
+            assert!(re.is_match(&branch_name), "branch name must begin with feature, improvement, bugfix, hotfix, hotfix-base, release, or deployment, then have a forward slash in the name.");
+        }
     }
-
-    // more program logic goes here...
 }
