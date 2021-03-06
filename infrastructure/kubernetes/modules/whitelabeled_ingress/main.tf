ff412bb92394e7bef343c21b121b8060f752c866
nick shook <nick@shook.family>
2021-03-05 17:51:20 -0800

    add k8 ingress whitelabeling (#1808)
    This commit includes whitelabeling logic for the following domains to
    point to the production interface:
    
    * www.deleteyourdata.com
    * www.deletetheirdata.com
    * www.justiceforrickieslaughter.com
    * www.shook.family

---
+++ infrastructure/kubernetes/modules/whitelabeled_ingress/main.tf
@@ -0,0 +1,60 @@
+resource "kubernetes_ingress" "primary" {
+  metadata {
+    name = "ingress"
+
+    annotations = {
+      "kubernetes.io/ingress.global-static-ip-name" = "neon-law"
+      "ingress.gcp.kubernetes.io/pre-shared-cert"   = "delete-your-data,delete-their-data,justice-for-rickie-slaughter,shook-family"
+    }
+  }
+
+  spec {
+    rule {
+      host = "www.deleteyourdata.com"
+      http {
+        path {
+          backend {
+            service_name = "production-interface"
+            service_port = 80
+          }
+        }
+      }
+    }
+
+    rule {
+      host = "www.deletetheirdata.com"
+      http {
+        path {
+          backend {
+            service_name = "production-interface"
+            service_port = 80
+          }
+        }
+      }
+    }
+
+    rule {
+      host = "www.justiceforrickieslaughter.com"
+      http {
+        path {
+          backend {
+            service_name = "production-interface"
+            service_port = 80
+          }
+        }
+      }
+    }
+
+    rule {
+      host = "www.shook.family"
+      http {
+        path {
+          backend {
+            service_name = "production-interface"
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
+}
