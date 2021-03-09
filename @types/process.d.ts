43f6894c9fa829648031860e3e26c0565d2d8607
nick shook <nick@shook.family>
2021-03-09 09:29:01 -0800

    remove new relic from k8 deployments (#1837)
    * remove new relic
    
    Instead of New Relic, we will opt for OpenTelemetry so we can use any
    provider to handle traces and logs.

--- @types/process.d.ts
+++ @types/process.d.ts
@@ -9,8 +9,6 @@ declare module "process" {
       AUTH0_CLIENT_SECRET?: string;
       AUTH0_CLIENT_TENANT?: string;
       GRAPHILE_LICENSE?: string;
-      NEW_RELIC_LICENSE_KEY?: string;
-      NEW_RELIC_NO_CONFIG_FILE?: 'true' | 'false' | undefined;
       SHOW_GRAPHIQL?: string;
     }
   }
