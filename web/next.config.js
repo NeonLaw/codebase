76cff6c9d35da8c1e777f677af86674e4080d2b8
nick shook <nick@shook.family>
2021-03-09 10:26:24 -0800

    remove rewrite logic from the next.js config (#1838)
    The rewrite logic did not work because Next will always look to the
    local `pages` directory first before proceeding with a rewrite rule.
    While we could break up the project into smaller chunks, it's okay for
    now for all pages to show all pages so long as the user knows it's Neon
    Law

--- web/next.config.js
+++ web/next.config.js
@@ -2,14 +2,6 @@ const withPlugins = require('next-compose-plugins');
 const withNextIntl = require('@moxy/next-intl/plugin');
 const withMDX = require('@next/mdx');
 
-const {
-  NEXT_PUBLIC_JUSTICE_FOR_RICKIE_SLAUGHTER_URL,
-  NEXT_PUBLIC_DELETE_YOUR_DATA_URL,
-  NEXT_PUBLIC_DELETE_THEIR_DATA_URL,
-  NEXT_PUBLIC_SHOOK_FAMILY_URL,
-  NEXT_PUBLIC_NEON_LAW_URL
-} = process.env;
-
 module.exports = withPlugins([
   withMDX(),
   withNextIntl(),
@@ -18,49 +10,6 @@ module.exports = withPlugins([
     environment: process.env.NODE_ENV
   },
   pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
-  async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-      return [];
-    }
-
-    return [
-      {
-        destination:
-          `https://${NEXT_PUBLIC_JUSTICE_FOR_RICKIE_SLAUGHTER_URL}/jfrs/:path*`,
-        source: '/jfrs/:path*',
-      },
-      {
-        destination: `https://${NEXT_PUBLIC_SHOOK_FAMILY_URL}/sf/:path*`,
-        source: '/sf/:path*',
-      },
-      {
-        destination:
-        `https://${NEXT_PUBLIC_DELETE_YOUR_DATA_URL}/delete-your/:path*`,
-        source: '/delete-your/:path*',
-      },
-      {
-        destination:
-        `https://${NEXT_PUBLIC_DELETE_THEIR_DATA_URL}/delete-their/:path*`,
-        source: '/delete-their/:path*',
-      },
-      {
-        destination: `https://${NEXT_PUBLIC_NEON_LAW_URL}/blog/:path*`,
-        source: '/blog/:path*',
-      },
-      {
-        destination: `https://${NEXT_PUBLIC_NEON_LAW_URL}/portal/:path*`,
-        source: '/portal/:path*',
-      },
-      {
-        destination: `https://${NEXT_PUBLIC_NEON_LAW_URL}/templates/:path*`,
-        source: '/templates/:path*',
-      },
-      {
-        destination: `https://${NEXT_PUBLIC_NEON_LAW_URL}/:path*`,
-        source: '/:path*',
-      },
-    ];
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.m4a$/,
