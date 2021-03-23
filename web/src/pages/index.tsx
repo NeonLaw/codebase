7b46349419ebe654045d274d5a6206cdbc4773d2
nick shook <nick@shook.family>
2021-03-23 13:10:49 -0700

    attempt to redirect the user based on serverSideProps

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,34 +1,35 @@
 import { NeonLawIndexPage } from '../components/indexPages/neonLawIndexPage';
-import Router from 'next/router';
 
-const Home = ({ host }) => {
-  if (host === 'www.justiceforrickieslaughter.com') {
-    Router.push(
-      'https://www.neonlaw.com/pro-bono/justice-for-rickie-slaughter'
-    );
-    return null;
-  }
-
-  if (host === 'www.deleteyourdata.com') {
-    Router.push('https://www.neonlaw.com/delete-your-data');
-    return null;
-  }
-
+const Home = () => {
   return <NeonLawIndexPage />;
 };
 
 /* eslint-disable-next-line import/no-default-export */
 export default Home;
 
-export const getServerSideProps = async ({ req, res }) => {
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=1, stale-while-revalidate=59'
-  );
+export const getServerSideProps = async ({ req }) => {
+  const host = req.headers.host;
+
+  if (host === 'www.justiceforrickieslaughter.com') {
+    return {
+      redirect: {
+        destination:
+          '/https://www.neonlaw.compro-bono/justice-for-rickie-slaughter',
+        permanent: true,
+      }
+    };
+  }
+
+  if (host === 'www.deleteyourdata.com') {
+    return {
+      redirect: {
+        destination: 'https://www.neonlaw.com/delete-your-data',
+        permanent: true,
+      }
+    };
+  }
 
   return {
-    props: {
-      host: req.headers.host
-    }
+    props: {}
   };
 };
