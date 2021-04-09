96b35728c04a4fe03dc63f91d74e98f547cb1a2d
nick shook <nick@shook.family>
2021-04-09 06:29:59 -0700

    fix warnings and error

--- server/tests/utils/db/insertPersonFixture.ts
+++ server/tests/utils/db/insertPersonFixture.ts
@@ -2,7 +2,7 @@ import * as faker from 'faker';
 
 export const insertPersonFixture = async (client: any) => {
   const email = faker.internet.email();
-  const sub = faker.random.uuid();
+  const sub = faker.datatype.uuid();
 
   const result = await client.query(
     'INSERT INTO person (email, sub) ' +
