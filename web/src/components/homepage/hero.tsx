7bd6b369c9747b4a808ec1d56991dac84e938c9c
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-18 07:51:50 +0500

    wip.

--- web/src/components/homepage/hero.tsx
+++ web/src/components/homepage/hero.tsx
@@ -1,61 +1,109 @@
-/* eslint-disable */
-// @ts-nocheck
-/* eslint-enable */
-import { Box, Heading, Text } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Spacer, Text, useColorMode, VStack } from '@chakra-ui/react';
 import { colors, gutters, sizes } from '../../styles/neonLaw';
-import { Button } from '../button';
 import { Container } from '../container';
 import React from 'react';
-import { theme } from '@chakra-ui/react';
 import { useIntl } from 'react-intl';
-import { useRouter } from 'next/router';
+import { TypeAhead } from '../typeAhead';
+import { useUser } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+
+const StyledHeadline = styled.div`
+  margin-bottom: ${gutters.small};
+
+  h1 {
+    font-size: 4.5rem;
+    font-weight: 400;
+  }
+
+  p {
+    font-size: 1rem;
+    text-transform: uppercase;
+    font-weight: 300;
+    letter-spacing: 3px;
+  }
+`
 
 export const Hero = () => {
   const intl = useIntl();
-  const router = useRouter();
+  const { user } = useUser();
+  const { colorMode } = useColorMode();
 
   return (
-    <Box
-      as="section"
+    <Container>
+      <Box
       display="flex"
       alignItems="center"
-      maxHeight="1200px"
+      justifyContent="center"
+      maxHeight="900px"
       minHeight="600px"
       height="100vh"
-      color={colors.text.dark}
+      width="100%"
+      background={colors.background[colorMode]}
+      color={colors.text[colorMode]}
       backgroundSize="cover"
       backgroundPosition="left"
       backgroundAttachment="fixed"
     >
-      <Container>
-        <Box maxWidth={['400px', '500px', sizes.textContainerSmall]}>
-          <Heading
-            as="h1"
-            fontSize={[
-              theme.fontSizes['xl1'],
-              theme.fontSizes['xl0'],
-              theme.fontSizes.xl,
-            ]}
-            fontWeight="400"
+          <VStack
+            maxWidth={['400px', '500px', '751px']}
+            textAlign="center"
           >
-            {intl.formatMessage({ id: 'banner.title' })}
-          </Heading>
-          <Text
-            margin={`${gutters.xSmall} 0 ${gutters.small}`}
-            color={colors.text.darkLight}
-          >
-            {intl.formatMessage({ id: 'banner.text' })}
-          </Text>
-          <Button
-            flash={true}
-            onClick={() => router.push('/contact')}
-            bg={colors.primaryColor900}
-            _hover={{ bg: colors.primaryColor800 }}
-          >
-            {intl.formatMessage({ id: 'auth.sign_up' })}
-          </Button>
-        </Box>
-      </Container>
+            <Box>
+              <Flex alignItems="center" direction="column">
+                <StyledHeadline>
+                  <h1>
+                    {intl.formatMessage({ id: 'banner.h1' })}
+                  </h1>
+                  <p>
+                    {intl.formatMessage({ id: 'banner.title' })}
+                  </p>
+                </StyledHeadline>
+              </Flex>
+              <TypeAhead />
+              <Text
+                margin={`${gutters.xSmall} 0 ${gutters.small}`}
+                color={colors.text[colorMode]}
+                display={['none', 'none', 'inherit']}
+                dangerouslySetInnerHTML={{
+                  __html: intl.formatMessage({ id: 'banner.text' }),
+                }}
+              />
+              <Text>
+                <span>{intl.formatMessage({ id: 'banner.client' })}</span>
+                &nbsp;
+                {user ? (
+                  <>
+                    <Link href="/portal" passHref={true}>
+                      <a href="/portal" style={{ textDecoration: 'underline' }}>
+                        {intl.formatMessage({ id: 'banner.view_matters' })}
+                      </a>
+                    </Link>
+                    <span>&nbsp;or&nbsp;</span>
+                    <Link href="/api/auth/logout" passHref={true}>
+                      <a
+                        href="/api/auth/logout"
+                        style={{ textDecoration: 'underline' }}
+                      >
+                        {intl.formatMessage({ id: 'banner.logout' })}
+                      </a>
+                    </Link>
+                  </>
+                ) : (
+                  <Link href="/api/auth/login">
+                    <a
+                      href="/api/auth/login"
+                      style={{ textDecoration: 'underline' }}
+                    >
+                      {intl.formatMessage({ id: 'banner.signin' })}
+                    </a>
+                  </Link>
+                )}
+                .
+              </Text>
+            </Box>
+          </VStack>
     </Box>
+    </Container>
   );
 };
--- web/src/components/indexPages/neonLawIndexPage.tsx
+++ web/src/components/indexPages/neonLawIndexPage.tsx
@@ -8,98 +8,20 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react';
-import { colors, gutters, sizes } from '../../styles/neonLaw';
 
-import { BaseFooter } from '../footer/baseFooter';
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import Link from 'next/link';
-import { ReactSVG } from 'react-svg';
 import { Seo } from '../seo';
-import { TypeAhead } from '../typeAhead';
-import { useColorMode } from '@chakra-ui/react';
-import { useIntl } from 'react-intl';
-import { useRef } from 'react';
-import { useUser } from '@auth0/nextjs-auth0';
+import { Hero } from '../homepage/hero';
+import { PublicLayout } from '../layouts/publicLayout';
 
 export const NeonLawIndexPage = (): JSX.Element => {
-  const { colorMode } = useColorMode();
-  const intl = useIntl();
 
-  const { user } = useUser();
-  const nextSectionRef = useRef(null);
+  // const nextSectionRef = useRef(null);
 
   return (
-    <>
+    <PublicLayout>
       <Seo title="Homepage" />
-      <Center
-        background={colors.background[colorMode]}
-        as="main"
-        display="flex"
-        alignItems="center"
-        height="100vh"
-        color={colors.text[colorMode]}
-      >
-        <VStack maxWidth={['400px', '500px', sizes.textContainerSmall]}>
-          <Box width="100%">
-            <Flex alignItems="center" height="250px">
-              <Spacer />
-              <ReactSVG src="/images/logo.svg" />
-              <Box>
-                <Heading as="h1" fontWeight="400" justifyItems="center">
-                  {intl.formatMessage({ id: 'banner.h1' })}
-                </Heading>
-                <Heading as="h4" fontWeight="400" justifyItems="center">
-                  {intl.formatMessage({ id: 'banner.title' })}
-                </Heading>
-              </Box>
-              <Spacer />
-            </Flex>
-            <TypeAhead />
-            <Text
-              margin={`${gutters.xSmall} 0 ${gutters.small}`}
-              color={colors.text[colorMode]}
-              display={['none', 'none', 'inherit']}
-              dangerouslySetInnerHTML={{
-                __html: intl.formatMessage({ id: 'banner.text' }),
-              }}
-            />
-            <Text>
-              <span>{intl.formatMessage({ id: 'banner.client' })}</span>
-              &nbsp;
-              {user ? (
-                <>
-                  <Link href="/portal" passHref={true}>
-                    <a href="/portal" style={{ textDecoration: 'underline' }}>
-                      {intl.formatMessage({ id: 'banner.view_matters' })}
-                    </a>
-                  </Link>
-                  <span>&nbsp;or&nbsp;</span>
-                  <Link href="/api/auth/logout" passHref={true}>
-                    <a
-                      href="/api/auth/logout"
-                      style={{ textDecoration: 'underline' }}
-                    >
-                      {intl.formatMessage({ id: 'banner.logout' })}
-                    </a>
-                  </Link>
-                </>
-              ) : (
-                <Link href="/api/auth/login">
-                  <a
-                    href="/api/auth/login"
-                    style={{ textDecoration: 'underline' }}
-                  >
-                    {intl.formatMessage({ id: 'banner.signin' })}
-                  </a>
-                </Link>
-              )}
-              .
-            </Text>
-            <Box width="100%" height="100px" />
-          </Box>
-        </VStack>
-      </Center>
-      <Flex marginTop="-3em" width="100%">
+      <Hero />
+      {/* <Flex marginTop="-3em" width="100%">
         <IconButton
           aria-label="Learn More"
           margin="0 auto"
@@ -108,10 +30,7 @@ export const NeonLawIndexPage = (): JSX.Element => {
             nextSectionRef.current.scrollIntoView({ behavior: 'smooth' });
           }}
         />
-      </Flex>
-      <Box ref={nextSectionRef} marginTop="3em">
-        <BaseFooter />
-      </Box>
-    </>
+      </Flex> */}
+    </PublicLayout>
   );
 };
