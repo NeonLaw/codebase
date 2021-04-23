a8e268db1834f4a8579d7feb14323e79c6823400
nick shook <nick@shook.family>
2021-04-23 13:24:43 -0700

    switch test order

--- postgres/spec/person/select_person_spec.rb
+++ postgres/spec/person/select_person_spec.rb
@@ -3,8 +3,6 @@ RSpec.describe "select from person" do
   let(:people) { connection[:person] }
 
   context "with no people" do
-    before { people.delete }
-
     it "returns an empty collection" do
       expect(people.all.count).to eq 0
     end
