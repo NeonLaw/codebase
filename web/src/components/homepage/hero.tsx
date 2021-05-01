583f7ed94700e3e1e241959409f6ba01299ac9c0
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-05-01 21:46:32 +0500

    Better Home Page Styling. (#2266)

--- web/src/components/homepage/hero.tsx
+++ web/src/components/homepage/hero.tsx
@@ -1,61 +1,108 @@
-/* eslint-disable */
-// @ts-nocheck
-/* eslint-enable */
-import { Box, Heading, Text } from '@chakra-ui/react';
-import { colors, gutters, sizes } from '../../styles/neonLaw';
-import { Button } from '../button';
+import { Box, Flex, Text, VStack, useColorMode } from '@chakra-ui/react';
+import { colors, gutters } from '../../styles/neonLaw';
 import { Container } from '../container';
-import React from 'react';
-import { theme } from '@chakra-ui/react';
+import Link from 'next/link';
+import { TypeAhead } from '../typeAhead';
+import styled from '@emotion/styled';
 import { useIntl } from 'react-intl';
-import { useRouter } from 'next/router';
+import { useUser } from '@auth0/nextjs-auth0';
+
+const StyledHeadline = styled.div`
+  margin-bottom: ${gutters.small};
+
+  h1 {
+    font-size: 4.5rem;
+    font-weight: 400;
+  }
+
+  p {
+    font-size: 1rem;
+    text-transform: uppercase;
+    font-weight: 300;
+    letter-spacing: 3px;
+  }
+`;
 
 export const Hero = () => {
   const intl = useIntl();
-  const router = useRouter();
+  const { user } = useUser();
+  const { colorMode } = useColorMode();
 
   return (
-    <Box
-      as="section"
-      display="flex"
-      alignItems="center"
-      maxHeight="1200px"
-      minHeight="600px"
-      height="100vh"
-      color={colors.text.dark}
-      backgroundSize="cover"
-      backgroundPosition="left"
-      backgroundAttachment="fixed"
-    >
-      <Container>
-        <Box maxWidth={['400px', '500px', sizes.textContainerSmall]}>
-          <Heading
-            as="h1"
-            fontSize={[
-              theme.fontSizes['xl1'],
-              theme.fontSizes['xl0'],
-              theme.fontSizes.xl,
-            ]}
-            fontWeight="400"
-          >
-            {intl.formatMessage({ id: 'banner.title' })}
-          </Heading>
-          <Text
-            margin={`${gutters.xSmall} 0 ${gutters.small}`}
-            color={colors.text.darkLight}
-          >
-            {intl.formatMessage({ id: 'banner.text' })}
-          </Text>
-          <Button
-            flash={true}
-            onClick={() => router.push('/contact')}
-            bg={colors.primaryColor900}
-            _hover={{ bg: colors.primaryColor800 }}
-          >
-            {intl.formatMessage({ id: 'auth.sign_up' })}
-          </Button>
-        </Box>
-      </Container>
-    </Box>
+    <Container>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        maxHeight="900px"
+        minHeight="600px"
+        height="100vh"
+        width="100%"
+        background={colors.background[colorMode]}
+        color={colors.text[colorMode]}
+        backgroundSize="cover"
+        backgroundPosition="left"
+        backgroundAttachment="fixed"
+      >
+        <VStack
+          maxWidth={['400px', '500px', '751px']}
+          textAlign="center"
+        >
+          <Box>
+            <Flex alignItems="center" direction="column">
+              <StyledHeadline>
+                <h1>
+                  {intl.formatMessage({ id: 'banner.h1' })}
+                </h1>
+                <p>
+                  {intl.formatMessage({ id: 'banner.title' })}
+                </p>
+              </StyledHeadline>
+            </Flex>
+            <TypeAhead />
+            <Text
+              margin={`${gutters.xSmall} 0 ${gutters.small}`}
+              color={colors.text[colorMode]}
+              display={['none', 'none', 'inherit']}
+              dangerouslySetInnerHTML={{
+                __html: intl.formatMessage({ id: 'banner.text' }),
+              }}
+            />
+            <Text>
+              <span>{intl.formatMessage({ id: 'banner.client' })}</span>
+                &nbsp;
+              {user ? (
+                <>
+                  <Link href="/portal" passHref={true}>
+                    <a href="/portal" style={{ textDecoration: 'underline' }}>
+                      {intl.formatMessage({ id: 'banner.view_matters' })}
+                    </a>
+                  </Link>
+                  <span>&nbsp;or&nbsp;</span>
+                  <Link href="/api/auth/logout" passHref={true}>
+                    <a
+                      href="/api/auth/logout"
+                      style={{ textDecoration: 'underline' }}
+                    >
+                      {intl.formatMessage({ id: 'banner.logout' })}
+                    </a>
+                  </Link>
+                </>
+              ) : (
+                <Link href="/api/auth/login">
+                  <a
+                    href="/api/auth/login"
+                    style={{ textDecoration: 'underline' }}
+                  >
+                    {intl.formatMessage({ id: 'banner.signin' })}
+                  </a>
+                </Link>
+              )}
+                .
+            </Text>
+          </Box>
+        </VStack>
+      </Box>
+    </Container>
   );
 };
--- web/src/components/typeAhead.tsx
+++ web/src/components/typeAhead.tsx
@@ -1,8 +1,38 @@
 import { gql, useLazyQuery } from '@apollo/client';
+import { AiOutlineSearch } from 'react-icons/ai';
 import { Box } from '@chakra-ui/react';
-import { Input } from '@chakra-ui/react';
 import Link from 'next/link';
+import { gutters } from '../styles/neonLaw';
+import styled from '@emotion/styled';
 import { useState } from 'react';
+
+
+const StyledInput = styled.input`
+  display: inline-block;
+  width: 100%;
+  padding: ${gutters.xSmallOne} ${gutters.small};
+  border: 1px solid #ddd;
+  box-shadow: 0 10px 20px rgba(0,0,0, .10);
+  border-radius: 3px;
+
+  &::placeholder {
+    font-size: 20px;
+    color: #555;
+    font-weight: 300;
+  }
+`;
+
+const StyledInputWrapper = styled.div`
+  position: relative;
+
+  .icon {
+    position: absolute;
+    top: 50%;
+    right: ${gutters.small};
+    transform: translateY(-50%);
+    color: #bbb;
+  }
+`;
 
 const QUERY = gql`
   query Questions {
@@ -66,13 +96,14 @@ export const TypeAhead = () => {
 
 
   return (
-    <>
-      <Input
+    <StyledInputWrapper>
+      <StyledInput
         type="text"
         value={text}
         placeholder="What legal question do you have?"
         onChange={onTextChange}
       />
-    </>
+      <AiOutlineSearch className="icon" size="1.5rem"/>
+    </StyledInputWrapper>
   );
 };
