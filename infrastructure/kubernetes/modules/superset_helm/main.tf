bce628782970b4fd9091046b504075b28ee370df
nick shook <nick@shook.family>
2021-05-02 00:38:31 -0700

    use the superset helm chart (#2273)

--- infrastructure/kubernetes/modules/superset_helm/main.tf
+++ infrastructure/kubernetes/modules/superset_helm/main.tf
@@ -1,5 +1,11 @@
 resource "helm_release" "superset" {
-  name   = "${var.environment}-superset"
-  chart  = "https://raw.githubusercontent.com/preset-io/superset/master/helm/superset/Chart.yaml"
-  verify = false
+  name       = "${var.environment}-superset"
+  repository = "https://apache.github.io/superset"
+  chart      = "superset"
+  verify     = false
+
+  set {
+    name  = "additionalRequirements"
+    value = jsonencode(["psycopg2", "elasticsearch-dbapi", "Authlib", "gevent"])
+  }
 }
