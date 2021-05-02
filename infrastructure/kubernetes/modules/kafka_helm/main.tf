0bd398c11c7f436bc121edbbf94eb81742590c3e
nick shook <nick@shook.family>
2021-05-02 00:26:52 -0700

    rename kafka helm

--- infrastructure/kubernetes/modules/kafka_helm/main.tf
+++ infrastructure/kubernetes/modules/kafka_helm/main.tf
@@ -1,4 +1,4 @@
-resource "helm_release" "neo4j" {
+resource "helm_release" "confluent-cloud" {
   name  = "${var.environment}-kafka"
   chart = "https://github.com/confluentinc/cp-helm-charts/releases/download/v6.0.1-1/cp-helm-charts-0.6.0.tgz"
 }
