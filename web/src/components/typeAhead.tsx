473ccd65a06b86064f8bd5249dd0e140b2d827b3
nisarhassan12 <syednisarhassan12@gmail.com>
2021-05-04 01:08:56 +0500

    focus the type ahead input when "/" is pressed.

--- web/src/components/typeAhead.tsx
+++ web/src/components/typeAhead.tsx
@@ -4,21 +4,25 @@ import { Box } from '@chakra-ui/react';
 import Link from 'next/link';
 import { gutters } from '../styles/neonLaw';
 import styled from '@emotion/styled';
-import { useState } from 'react';
-
+import { useState, useRef, useEffect } from 'react';
 
 const StyledInput = styled.input`
   display: inline-block;
   width: 100%;
   padding: ${gutters.xSmallOne} ${gutters.small};
-  border: 1px solid #ddd;
+  border: 1px solid #ddd ;
   box-shadow: 0 10px 20px rgba(0,0,0, .10);
   border-radius: 3px;
+  transition: all .2s;
 
   &::placeholder {
     font-size: 20px;
     color: #555;
     font-weight: 300;
+  }
+
+  &:focus {
+    box-shadow: 0 12px 22px rgba(0,0,0, .15);
   }
 `;
 
@@ -49,6 +53,28 @@ export const TypeAhead = () => {
   const [getQuestions, { loading, data }] = useLazyQuery(QUERY);
   const [questions, setQuestions] = useState([]);
   const [text, setText] = useState('');
+  const inputRef = useRef<any>();
+
+  const handleSlashPress = (e) => {
+    if (e.key === '/') {
+      console.log(e.key);
+      const activeEl = document.activeElement;
+      const focusWrapper = document.querySelector('#focus-wrapper');
+
+      if (activeEl === focusWrapper || activeEl === document.body) {
+        e.preventDefault();
+        inputRef.current?.focus({ preventScroll: true });
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleSlashPress);
+
+    return (): void => {
+      window.removeEventListener('keydown', handleSlashPress);
+    };
+  }, []);
 
   if (loading) return <p>Loading...</p>;
 
@@ -63,7 +89,7 @@ export const TypeAhead = () => {
             }
             return (
               <>
-                <li key={index}>
+                <li key={index} data-testId="question">
                   <Link href={`/questions/${question.id}`}>
                     {question.prompt}
                   </Link>
@@ -98,6 +124,8 @@ export const TypeAhead = () => {
   return (
     <StyledInputWrapper>
       <StyledInput
+        name="type-ahead"
+        ref={inputRef}
         type="text"
         value={text}
         placeholder="What legal question do you have?"
