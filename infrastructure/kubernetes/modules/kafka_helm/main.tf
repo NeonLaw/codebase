74341317e2684b02e16c0f862801b912ad2bde43
nick shook <nick@shook.family>
2021-05-04 12:28:32 -0700

    fix helm version in kafka diagram

--- infrastructure/kubernetes/modules/kafka_helm/main.tf
+++ infrastructure/kubernetes/modules/kafka_helm/main.tf
@@ -2,5 +2,5 @@ resource "helm_release" "kafka" {
   name       = "${var.environment}-kafka"
   repository = "https://confluentinc.github.io/cp-helm-charts"
   chart      = "confluentinc/cp-helm-charts"
-  version    = "0.6.0"
+  version    = "6.0.1"
 }
