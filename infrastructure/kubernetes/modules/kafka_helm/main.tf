5d53e2d704c9ae3996aac50157b9fc005f974040
nick shook <nick@shook.family>
2021-05-04 13:07:33 -0700

    try again with cp-helm-charts (#2301)

--- infrastructure/kubernetes/modules/kafka_helm/main.tf
+++ infrastructure/kubernetes/modules/kafka_helm/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "kafka" {
   name       = "${var.environment}-kafka"
   repository = "https://confluentinc.github.io/cp-helm-charts"
-  chart      = "confluentinc/cp-helm-charts"
-  version    = "6.0.1"
+  chart      = "cp-helm-charts"
+  version    = "0.6.0"
 }
