1d2a0ba91e5702cd2977111621b65a4cc1168911
nick shook <nick@shook.family>
2021-05-04 21:16:45 -0700

    use separate yaml encode

--- infrastructure/kubernetes/modules/superset_helm/main.tf
+++ infrastructure/kubernetes/modules/superset_helm/main.tf
@@ -1,19 +1,22 @@
+locals {
+  superset_helm_chart_values = {
+    ingress = {
+      enabled = true
+      annotations = {
+        "kubernetes.io/ingress.global-static-ip-name" = "neon-law"
+        "ingress.gcp.kubernetes.io/pre-shared-cert"   = "neon-law-superset"
+      }
+    }
+  }
+}
+
 resource "helm_release" "superset" {
   name       = "${var.environment}-superset"
   repository = "https://apache.github.io/superset"
   chart      = "superset"
   verify     = false
 
-  set {
-    name  = "ingress.enabled"
-    value = "true"
-  }
-
-  set {
-    name = "ingress.annotations"
-    value = jsonencode({
-      "kubernetes.io/ingress.global-static-ip-name" = "neon-law"
-      "ingress.gcp.kubernetes.io/pre-shared-cert"   = "neon-law-superset"
-    })
-  }
+  values = [
+    yamlencode(local.superset_helm_chart_values)
+  ]
 }
