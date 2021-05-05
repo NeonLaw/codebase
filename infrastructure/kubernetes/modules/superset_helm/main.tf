cc1d42dd60bf31113855d0cc529959b4a5bf08b3
nick shook <nick@shook.family>
2021-05-04 19:14:32 -0700

    add superset helm config

--- infrastructure/kubernetes/modules/superset_helm/main.tf
+++ infrastructure/kubernetes/modules/superset_helm/main.tf
@@ -3,4 +3,17 @@ resource "helm_release" "superset" {
   repository = "https://apache.github.io/superset"
   chart      = "superset"
   verify     = false
+
+  set {
+    name  = "ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name = "ingress.annotations"
+    value = {
+      "kubernetes.io/ingress.global-static-ip-name" = "neon-law"
+      "ingress.gcp.kubernetes.io/pre-shared-cert"   = "neon-law-superset"
+    }
+  }
 }
