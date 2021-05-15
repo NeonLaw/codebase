77f887232c6e6a4c3c3175d39ca6c59bb2813ebe
nisarhassan12 <syednisarhassan12@gmail.com>
2021-05-16 01:42:44 +0500

    improve the 404 page.

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -1,43 +1,53 @@
-import {
-  Box,
-  Button,
-  Link as ChakraLink,
-  Heading,
-  Image,
-  Text,
-  useColorMode,
-} from '@chakra-ui/react';
-import Link from 'next/link';
+import { Button } from '../components/button';
+import { PublicLayout } from '../components/layouts/publicLayout';
+import { gutters } from '../styles/neonLaw';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+
+const Styled404Page = styled.div`
+  text-align: center;
+  padding: 9rem 0 4rem;
+
+  .wrapper {
+    min-height: 50vh;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 8rem;
+
+    @media (max-width: 400px) {
+      font-size: 6rem;
+    }
+  }
+
+  p {
+    margin-bottom: ${gutters.xSmallOne};
+  }
+`;
 
 const Page404 = () => {
-  const { colorMode } = useColorMode();
+  const router = useRouter();
 
   return (
-    <>
-      <Box width={['100%', '70%', '60%', '60%']} margin="0 auto">
-        <Image src="/404 Error-pana.svg" />
-      </Box>
-      <Text textAlign="center" fontSize="xs">
-        <ChakraLink href="https://stories.freepik.com/web" isExternal>
-          Illustration by Freepik Stories
-        </ChakraLink>
-      </Text>
-
-      <Box marginY={4}>
-        <Heading textAlign="center">Page not Found.</Heading>
-
-        <Box textAlign="center" marginTop={4}>
-          <Text>It is Okay!</Text>
-          <Link href="/" passHref>
-            <Button
-              backgroundColor={colorMode === 'light' ? 'gray.300' : 'cyan.500'}
-            >
-              Let us Head Back
-            </Button>
-          </Link>
-        </Box>
-      </Box>
-    </>
+    <PublicLayout isFooterSectionHidden={true}>
+      <Styled404Page>
+        <div className="wrapper">
+          <h1>404</h1>
+          <p>Oh, no! Something went wrong on our side.</p>
+          <Button
+            onClick={() => {
+              router.push('/');
+            }}
+          >
+            Go Home
+          </Button>
+        </div>
+      </Styled404Page>
+    </PublicLayout>
   );
 };
 
