1a6cbe30daf87a03ffa726c74d948ca1c6eb179d
nick shook <nick@shook.family>
2021-05-20 19:10:20 -0700

    fix tests

--- infrastructure/gcp/modules/pub_sub/outputs.tf
+++ infrastructure/gcp/modules/pub_sub/outputs.tf
@@ -1,13 +1,3 @@
 output "name" {
-  value = google_sql_database_instance.postgres.name
-}
-
-output "admin_password" {
-  value = random_string.password.result
-  sensitive = true
-}
-
-output "admin_username" {
-  value = "neon_law_admin"
-  sensitive = true
+  value = "outbound-email"
 }
