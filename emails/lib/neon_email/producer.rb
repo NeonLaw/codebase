a57cb746087acc30e7eb7d4a8058c792011e4601
nick <nick@shook.family>
2021-05-23 13:18:08 -0700

    fix outbound-schema-helpers (#2429)

--- emails/lib/neon_email/producer.rb
+++ emails/lib/neon_email/producer.rb
@@ -1,20 +1,19 @@
 require "google/cloud/pubsub"
-require "avro"
+require "neon_schemas"
 
 module NeonEmail
   class Producer
     def self.produce_outbound_email
-      pubsub = Google::Cloud::PubSub.new(
-        project_id: "neon-law-staging",
-        credentials: "#{__dir__}/../../../neon-law-staging.credentials.json"
-      )
+      Google::Cloud::PubSub.configure do |config|
+        config.project_id = ENV.fetch("GCP_PROJECT_ID")
+        config.credentials = JSON.parse(ENV.fetch("GCP_CREDENTIALS"))
+      end
+
+      pubsub = Google::Cloud::PubSub.new
 
       # Retrieve a topic
       topic = pubsub.topic "outbound_email"
 
-      avro_schema = Avro::Schema.parse(
-        File.read("#{__dir__}/../../../schemas/src/outbound_email.avsc")
-      )
       record = {
         email: "hello@rar",
         email_type: {
@@ -22,18 +21,18 @@ module NeonEmail
           sub: "rar"
         }
       }
-      writer = Avro::IO::DatumWriter.new avro_schema
-      buffer = StringIO.new
-      encoder = Avro::IO::BinaryEncoder.new buffer
 
-      writer.write record, encoder
+      encoded_message = NeonSchemas::Avro.encode(
+        record: record,
+        schema_name: "outbound_email"
+      )
 
       # Publish a new message
-      topic.publish buffer
+      topic.publish encoded_message
+    rescue SignalException => e
+      puts "Received Signal #{e}"
+      exit
     end
-  rescue SignalException => e
-    puts "Received Signal #{e}"
-    exit
   end
 end
 
