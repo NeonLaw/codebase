71e4583e221cfae03428c60040a161175881876f
nick shook <nick@shook.family>
2021-05-25 00:34:51 -0700

    add namespacing

--- postgres/spec/spec_helper.rb
+++ postgres/spec/spec_helper.rb
@@ -2,15 +2,7 @@ require "bundler/setup"
 require "neon_postgres"
 require "pry"
 require "faker"
-require "fixture_dependencies/rspec/sequel"
-
-Sequel::Model.db = NeonPostgres::Database.connection
-FixtureDependencies.fixture_path = "#{__dir__}/fixtures"
-FixtureDependencies.class_map[:person] = NeonPostgres::Models::Person
-FixtureDependencies.class_map[:document] = NeonPostgres::Models::Document
-FixtureDependencies.class_map[:document_template] = NeonPostgres::Models::DocumentTemplate
-FixtureDependencies.class_map[:matter_template] = NeonPostgres::Models::MatterTemplate
-FixtureDependencies.class_map[:matter] = NeonPostgres::Models::Matter
+require_relative './support/fixture_dependencies'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
