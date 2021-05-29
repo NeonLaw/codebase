0465180b5c5f72362587448ed4f63cab7186a974
nick shook <nick@shook.family>
2021-05-29 02:28:45 -0700

    add event handler

--- documents/lib/neon_documents.rb
+++ documents/lib/neon_documents.rb
@@ -1,5 +1,5 @@
 require "neon_documents/version"
-require "neon_documents/consumer"
+require "neon_documents/handler"
 require "neon_documents/document_processor"
 Dir["#{__dir__}/neon_documents/document_templates/*"].sort.each { |file|
   require file
