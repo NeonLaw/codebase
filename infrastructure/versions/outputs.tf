3a15d352b0e2b42cf385fd702f6e3f45bb849092
nick <nick@shook.family>
2021-05-30 20:11:47 -0700

    flat version outputs (#2463)

--- infrastructure/versions/outputs.tf
+++ infrastructure/versions/outputs.tf
@@ -1,3 +1,15 @@
-output "staging_green" {
-  value = data.null_data_source.versions.outputs.staging_green
+output "staging_green_schemas" {
+  value = data.null_data_source.versions.outputs.staging_green_schemas
+}
+
+output "staging_blue_schemas" {
+  value = data.null_data_source.versions.outputs.staging_green_schemas
+}
+
+output "production_green_schemas" {
+  value = data.null_data_source.versions.outputs.production_green_schemas
+}
+
+output "production_blue_schemas" {
+  value = data.null_data_source.versions.outputs.production_green_schemas
 }
