1470a1f83ee84aba6bfd3f6cd63561d486acfbdd
nick shook <nick@shook.family>
2021-06-01 12:08:03 -0700

    only one string for a member

--- infrastructure/gcp/modules/container_registry/main.tf
+++ infrastructure/gcp/modules/container_registry/main.tf
@@ -6,7 +6,5 @@ resource "google_container_registry" "registry" {
 resource "google_storage_bucket_iam_member" "viewer" {
   bucket = google_container_registry.registry.id
   role   = "roles/storage.objectViewer"
-  member = [
-    "serviceAccount:application-user@${var.project_id}.iam.gserviceaccount.com"
-  ]
+  member = "serviceAccount:application-user@${var.project_id}.iam.gserviceaccount.com"
 }
