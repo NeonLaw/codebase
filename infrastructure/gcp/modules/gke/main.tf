b9d553f2ddffb85bff4416b5c30503b10d196564
nick shook <nick@shook.family>
2021-06-02 08:28:15 -0700

    brink back gke cluster
    deleted and recreated from scratch to get around IAM issues

--- infrastructure/gcp/modules/gke/main.tf
+++ infrastructure/gcp/modules/gke/main.tf
@@ -1,31 +1,20 @@
-resource "random_string" "random" {
-  length  = 16
-  special = false
+resource "google_container_cluster" "primary" {
+  provider   = google-beta
+  name       = "neon-law-${var.environment}"
+  location   = var.region
+  network    = "projects/${var.project_id}/global/networks/default"
+  subnetwork = "default"
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = true
+    }
+  }
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = ""
+    services_ipv4_cidr_block = ""
+  }
+
+  enable_autopilot = true
 }
-
-resource "random_password" "password" {
-  length           = 16
-  special          = true
-  override_special = "_"
-}
-
-# resource "google_container_cluster" "primary" {
-#   provider   = google-beta
-#   name       = "neon-law-${var.environment}"
-#   location   = var.region
-#   network    = "projects/${var.project_id}/global/networks/default"
-#   subnetwork = "default"
-
-#   master_auth {
-#     client_certificate_config {
-#       issue_client_certificate = true
-#     }
-#   }
-
-#   ip_allocation_policy {
-#     cluster_ipv4_cidr_block  = ""
-#     services_ipv4_cidr_block = ""
-#   }
-
-#   enable_autopilot = true
-# }
