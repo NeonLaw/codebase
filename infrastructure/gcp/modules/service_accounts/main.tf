f39fdba2ec57b0ff85df661e0ad9475f00153531
nick shook <nick@shook.family>
2021-06-02 12:25:00 -0700

    *NO PR* service account update for emergency TF

--- infrastructure/gcp/modules/service_accounts/main.tf
+++ infrastructure/gcp/modules/service_accounts/main.tf
@@ -8,16 +8,6 @@ resource "google_service_account_key" "application_user_account_key" {
   service_account_id = google_service_account.application_user_account.name
 }
 
-resource "google_service_account_iam_binding" "applicaiton_user_iam_binding" {
-  service_account_id = google_service_account.application_user_account.name
-  role               = "roles/iam.serviceAccountUser"
-
-  members = [
-    "user:nick@neonlaw.com",
-    "serviceAccount:terraform@${var.project_id}.iam.gserviceaccount.com",
-  ]
-}
-
 resource "google_project_iam_binding" "editor_bindings" {
   project = var.project_id
   role    = "roles/editor"
@@ -39,16 +29,6 @@ resource "google_project_iam_binding" "kubernetes_admin_bindings" {
   ]
 }
 
-resource "google_service_account_iam_binding" "compute_user_iam_binding" {
-  service_account_id = "${var.project_number}-compute@developer.gserviceaccount.com"
-  role               = "roles/iam.serviceAccountUser"
-
-  members = [
-    "user:nick@neonlaw.com",
-    "serviceAccount:${var.project_number}@cloudservices.gserviceaccount.com"
-  ]
-}
-
 resource "google_project_iam_binding" "compute_admin_bindings" {
   project = var.project_id
   role    = "roles/compute.admin"
@@ -64,6 +44,8 @@ resource "google_project_iam_binding" "service_account_user_bindings" {
   role    = "roles/iam.serviceAccountUser"
 
   members = [
-    "serviceAccount:application-user@${var.project_id}.iam.gserviceaccount.com",
+    "serviceAccount:${var.project_number}@cloudservices.gserviceaccount.com",
+    "user:nick@neonlaw.com",
+    "serviceAccount:terraform@${var.project_id}.iam.gserviceaccount.com",
   ]
 }
