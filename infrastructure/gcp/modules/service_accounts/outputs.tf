00a13fb1ddde231d102bc303cbb2e35c0a1e6ddf
nick shook <nick@shook.family>
2021-06-02 19:31:25 -0700

    add service account for GitHub actions to TF
    Also the single-source-of-truth key is created in TF that can then be
    used in GitHub Actions.

--- infrastructure/gcp/modules/service_accounts/outputs.tf
+++ infrastructure/gcp/modules/service_accounts/outputs.tf
@@ -1,3 +1,7 @@
 output "application_user_account_key" {
   value = google_service_account_key.application_user_account_key.private_key
 }
+
+output "github_actions_account_key" {
+  value = google_service_account_key.github_actions_account_key.private_key
+}
--- infrastructure/gcp/outputs.tf
+++ infrastructure/gcp/outputs.tf
@@ -60,6 +60,11 @@ output "application_user_account_key" {
   sensitive = true
 }
 
+output "github_actions_account_key" {
+  value     = module.service_accounts.github_actions_account_key
+  sensitive = true
+}
+
 output "database_name" {
   value = "neon-law"
 }
