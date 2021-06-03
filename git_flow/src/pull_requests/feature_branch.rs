bcac11a68c114015a6acbaea78f8f8d78df00973
nick shook <nick@shook.family>
2021-06-03 10:52:35 -0700

    add basic github check in git_flow runtime

--- git_flow/src/pull_requests/feature_branch.rs
+++ git_flow/src/pull_requests/feature_branch.rs
@@ -1,3 +1,21 @@
-pub fn validate_feature_branch(head_ref: &str) {
-    println!("Feature Branch Method was called. {}", &head_ref)
+use octocrab::{params};
+
+pub async fn validate_feature_branch(head_ref: &str) -> octocrab::Result<()> {
+    let octocrab = octocrab::Octocrab::builder()
+    .personal_token(std::env::var("GITHUB_PAT").unwrap())
+    .build()?;
+
+    match octocrab.issues("neonlaw", "codebase")
+        .list()
+        .creator("shicholas")
+        .state(params::State::All)
+        .per_page(50)
+        .send()
+        .await
+        {
+            Ok(page) => println!("{:#?}", page),
+            Err(error) => println!("{:#?}", error),
+        }
+
+    Ok(())
 }
