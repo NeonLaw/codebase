271309279cc85b56e257b535426ab1417beb6b58
nick <nick@shook.family>
2021-06-04 10:33:34 -0700

    switch from octocrab to hubcaps, better docs (#2500)
    * switch from octocrab to hubcaps, better docs

--- git_flow/src/main.rs
+++ git_flow/src/main.rs
@@ -9,9 +9,6 @@ use tokio::runtime;
 #[derive(Clap)]
 #[clap(version = "0.1", author = "Support <support@neonlaw.com>")]
 struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short, long, default_value = "~/.git_flow.conf")]
-    config: String,
     /// A level of verbosity, and can be used multiple times
     #[clap(subcommand)]
     subcmd: SubCommands,
@@ -54,13 +51,14 @@ struct ValidatePullRequest {
     /// Head branch or the "to" branch
     #[clap(long)]
     base_ref: String,
+
+    /// The PR Number
+    #[clap(long)]
+    pr_number: u64,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-
-    // Gets a value for config if supplied by user, or defaults to "git_flow.conf"
-    println!("Value for config: {}", opts.config);
 
     // You can handle information about subcommands by requesting their matches by name
     // (as below), requesting just the name used, or both at the same time
@@ -82,7 +80,8 @@ fn main() {
 
         SubCommands::ValidatePullRequest(t) => {
             let head_ref: String = t.head_ref;
-            println!("Validating Branch {}", head_ref);
+            let pr_number: u64 = t.pr_number;
+            println!("Validating head_ref {}", head_ref);
 
             let re = Regex::new(r"^(feature|improvement|bugfix|hotfix|hotfix-base|release|deployment|dependabot|version-bump)/.*$").unwrap();
             let caps = re.captures(&head_ref).unwrap();
@@ -90,11 +89,11 @@ fn main() {
 
             assert!(
                 re.is_match(&head_ref),
-                "the head ref branch name must begin with feature, improvement, bugfix, hotfix, hotfix-base, release, deployment, or dependabot and then have a forward slash in the name."
+                "the head_ref branch name must begin with feature, improvement, bugfix, hotfix, hotfix-base, release, deployment, or dependabot and then have a forward slash in the name."
             );
 
             if branch_type == "feature" {
-                let future = validate_feature_branch(&head_ref);
+                let future = validate_feature_branch(pr_number);
                 let rt = runtime::Builder::new_multi_thread()
                     .enable_all()
                     .build()
@@ -104,7 +103,7 @@ fn main() {
             }
 
             let base_ref: String = t.base_ref;
-            println!("Validating main {}", base_ref);
+            println!("Validating base_ref {}", base_ref);
             let main_branch = Regex::new(r"^main$").unwrap();
             assert!(
                 main_branch.is_match(&base_ref),
