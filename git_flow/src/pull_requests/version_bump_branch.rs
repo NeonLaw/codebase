fefe441ac294d9046526f598bf56556cb854b0d4
nick shook <nick@shook.family>
2021-06-07 07:06:55 -0700

    commits

--- git_flow/src/pull_requests/version_bump_branch.rs
+++ git_flow/src/pull_requests/version_bump_branch.rs
@@ -6,12 +6,12 @@ pub async fn validate_version_bump_branch(pr_number: u64) -> Result<(), Box<dyn 
   let token = env::var("GITHUB_PAT")?;
   let github = Github::new("github-application", Credentials::Token(token))?;
 
-  println!("comments");
+  println!("commits");
   for c in github
     .repo("neonlaw", "codebase")
     .pulls()
     .get(pr_number)
-    .comments()
+    .commits()
     .list(&Default::default())
     .await?
   {
