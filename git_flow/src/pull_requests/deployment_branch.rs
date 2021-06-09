5a91b8eb1a2096b9b78ac3390518d3be356e9a18
nick shook <nick@shook.family>
2021-06-09 08:53:14 -0700

    fix import

--- git_flow/src/pull_requests/deployment_branch.rs
+++ git_flow/src/pull_requests/deployment_branch.rs
@@ -1,8 +1,10 @@
-use octocrab;
-use std::env;
+use octocrab::{
+    Result,
+    Octocrab,
+};
 use std::error::Error;
 
-pub async fn validate_deployment_branch(pr_number: u64) -> octocrab::Result<(), Box<dyn Error>> {
+pub async fn validate_deployment_branch(pr_number: u64) -> Result<(), Box<dyn Error>> {
   let token = std::env::var("GITHUB_PAT").expect("GITHUB_PAT env variable is required");
 
   let octocrab = Octocrab::builder().personal_token(token).build()?;
