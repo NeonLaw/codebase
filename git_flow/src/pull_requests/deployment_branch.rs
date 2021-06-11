e662b14f9797afa893e198b4391fa555c7d9fac6
nick shook <nick@shook.family>
2021-06-11 12:22:27 -0700

    rename envvars

--- git_flow/src/pull_requests/deployment_branch.rs
+++ git_flow/src/pull_requests/deployment_branch.rs
@@ -5,7 +5,7 @@ use octocrab::{
 use std::error::Error;
 
 pub async fn validate_deployment_branch(pr_number: u64) -> Result<(), Box<dyn Error>> {
-  let token = std::env::var("GITHUB_PAT").expect("GITHUB_PAT env variable is required");
+  let token = std::env::var("GIT_PERSONAL_ACCESS_TOKEN").expect("GIT_PERSONAL_ACCESS_TOKEN env variable is required");
 
   let octocrab = Octocrab::builder().personal_token(token).build()?;
 
