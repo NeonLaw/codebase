0e0fd55e8d121e8cc4d8f91a002be5c3db569df9
nick <nick@shook.family>
2021-06-12 22:13:28 -0700

    bump emails to v0.0.7 (#2540)

--- infrastructure/versions/main.tf
+++ infrastructure/versions/main.tf
@@ -1,15 +1,15 @@
 data "null_data_source" "versions" {
   inputs = {
     staging_green_schemas = "0.1.1"
-    staging_green_emails = "0.1.3"
+    staging_green_emails = "0.0.7"
 
     staging_blue_schemas = "0.1.0"
-    staging_blue_emails = "0.1.3"
+    staging_blue_emails = "0.0.7"
 
     production_green_schemas = "0.1.1"
-    production_green_emails = "0.1.3"
+    production_green_emails = "0.0.7"
 
     production_blue_schemas = "0.1.0"
-    production_blue_emails = "0.1.3"
+    production_blue_emails = "0.0.7"
   }
 }
