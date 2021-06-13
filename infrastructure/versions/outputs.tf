58e5f41ac2ad320afda1511bf74e8cdbd5296052
nick shook <nick@shook.family>
2021-06-13 00:02:15 -0700

    add outputs to versions

--- infrastructure/versions/outputs.tf
+++ infrastructure/versions/outputs.tf
@@ -13,3 +13,19 @@ output "production_green_schemas" {
 output "production_blue_schemas" {
   value = data.null_data_source.versions.outputs.production_blue_schemas
 }
+
+output "staging_green_emails" {
+  value = data.null_data_source.versions.outputs.staging_green_emails
+}
+
+output "staging_blue_emails" {
+  value = data.null_data_source.versions.outputs.staging_blue_emails
+}
+
+output "production_green_emails" {
+  value = data.null_data_source.versions.outputs.production_green_emails
+}
+
+output "production_blue_emails" {
+  value = data.null_data_source.versions.outputs.production_blue_emails
+}
