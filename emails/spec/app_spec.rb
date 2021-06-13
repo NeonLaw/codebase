0acdb5653a69acc1907fe22844835227af522fcc
nick shook <nick@shook.family>
2021-06-13 16:13:30 -0700

    add basic logging spec for app.rb
    This spec uses the function framework to trigger a pubsub message and
    see it ingested.

--- emails/spec/app_spec.rb
+++ emails/spec/app_spec.rb
@@ -1,4 +1,18 @@
+require "functions_framework"
+
 RSpec.describe "app.rb" do
   context "with a welcome_email event" do
+    it "invokes the NeonEmail::Mailer#send_welcome_email" do
+      load_temporary "${__dir__}/../app.rb" do
+        event = make_cloud_event(
+          {yes: "yes"}.to_json,
+          type: "neon_law.welcome_email"
+        )
+
+        expect_any_instance_of(Logger).to receive(:info).with(event)
+
+        call_event("neon_law.welcome_email", event)
+      end
+    end
   end
 end
