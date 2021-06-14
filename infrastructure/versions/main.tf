e9d4b4dd6f0b193fbef3d5e7e70128c777c8d575
nick <nick@shook.family>
2021-06-14 11:31:20 -0700

    update email version to 0.0.9 (#2566)

--- infrastructure/versions/main.tf
+++ infrastructure/versions/main.tf
@@ -6,10 +6,10 @@ data "null_data_source" "versions" {
     staging_web = "0.1.1"
 
     staging_green_schemas = "0.1.5"
-    staging_green_emails = "0.0.8"
+    staging_green_emails = "0.0.9"
 
     staging_blue_schemas = "0.1.4"
-    staging_blue_emails = "0.0.8"
+    staging_blue_emails = "0.0.9"
 
     # Production Versions
     production_server = "0.1.3"
@@ -17,9 +17,9 @@ data "null_data_source" "versions" {
     production_web = "0.1.1"
 
     production_green_schemas = "0.1.5"
-    production_green_emails = "0.0.8"
+    production_green_emails = "0.0.9"
 
     production_blue_schemas = "0.1.4"
-    production_blue_emails = "0.0.8"
+    production_blue_emails = "0.0.9"
   }
 }
