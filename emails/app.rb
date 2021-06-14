4decec3762f9c7cbb07cac3e5769139658ec6562
nick <nick@shook.family>
2021-06-14 16:46:27 -0700

    parse 64 encoded message (#2567)
    when a message comes in from pubsub, it comes in as an object with a `:data` key. This key contains a base64 encoded dump of the avro-schemaed message.

--- emails/app.rb
+++ emails/app.rb
@@ -1,11 +1,15 @@
 require "functions_framework"
 require "neon_email"
 require "logger"
+require "base64"
+require "json"
 
 logger = Logger.new($stdout)
 
 WELCOME_EMAIL_TOPIC = "com.neon_law.outbound_email.welcome_email".freeze
 
 FunctionsFramework.cloud_event WELCOME_EMAIL_TOPIC do |event|
-  logger.info event.data
+  data = JSON.parse(Base64.strict_decode64(event.data.fetch(:data)))
+
+  logger.info(data)
 end
