ca79d9e9dd97bd0e5e50e1a127cccd5d5be3e676
nick <nick@shook.family>
2021-06-14 22:56:43 -0700

    allow functions to access secrets
    add permissions to the default GCP functions service account to access functions.

--- infrastructure/gcp/modules/functions/main.tf
+++ infrastructure/gcp/modules/functions/main.tf
@@ -11,5 +11,9 @@ resource "google_cloudfunctions_function" "welcome_email" {
   event_trigger {
     event_type = "google.pubsub.topic.publish"
     resource = "projects/${var.project_id}/topics/welcome-email-${var.schema_version}"
+
+    failure_policy {
+      retry = false
+    }
   }
 }
