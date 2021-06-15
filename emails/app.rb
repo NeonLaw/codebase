ff440a2b72b15d323ce0ad08b353544274880219
nick <nick@shook.family>
2021-06-14 19:57:49 -0700

    add message key (#2569)
    this fixes logic in GCP which requires a message key.

--- emails/app.rb
+++ emails/app.rb
@@ -9,7 +9,9 @@ logger = Logger.new($stdout)
 WELCOME_EMAIL_TOPIC = "com.neon_law.outbound_email.welcome_email".freeze
 
 FunctionsFramework.cloud_event WELCOME_EMAIL_TOPIC do |event|
-  data = JSON.parse(Base64.strict_decode64(event.data.fetch(:data)))
+  data = JSON.parse(
+    Base64.strict_decode64(event.data.fetch(:message).fetch(:data))
+  )
 
   logger.info(data)
 end
