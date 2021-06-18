bee4d6b5370548814861e348f8a283d6488be1bc
nick shook <nick@shook.family>
2021-06-18 00:57:43 -0400

    fix tests

--- emails/app.rb
+++ emails/app.rb
@@ -12,8 +12,9 @@ FunctionsFramework.cloud_event "outbound_emails.welcome_email" do |event|
   data = JSON.parse(
     Base64.strict_decode64(event.data.fetch("message").fetch("data"))
   )
-  to = data.fetch("to")
-  sub = data.fetch("sub")
+  to_email = data.fetch("to")
+
+  NeonEmail::GreetingMailer.deliver(to_email: to_email)
 
   logger.info(data)
 end
--- infrastructure/gcp/modules/dns/main.tf
+++ infrastructure/gcp/modules/dns/main.tf
@@ -1,3 +1,11 @@
-resource "google_dns" "record_set" {
+resource "google_dns_managed_zone" "neon_law" {
+  name        = "neon-law"
+  dns_name    = var.neon_law_url
+  description = "Neon Law DNS"
+}
 
+resource "google_dns_managed_zone" "delete_your_data" {
+  name        = "delete-your-data"
+  dns_name    = var.delete_your_data_url
+  description = "Delete Your Data DNS"
 }
