f8eca965880a9446ceb4f777819091d32aeb17a6
nick shook <nick@shook.family>
2021-06-18 01:53:28 -0400

    more tf fixes

--- infrastructure/gcp/modules/pubsub_topic/main.tf
+++ infrastructure/gcp/modules/pubsub_topic/main.tf
@@ -1,19 +1,19 @@
 data "http" "schema_definition" {
-  url = "https://raw.githubusercontent.com/NeonLaw/codebase/schemas%40${var.schema_version}/schemas/schemas/${var.topic_name}.avsc"
+  url = "https://raw.githubusercontent.com/NeonLaw/codebase/schemas%40${var.schema_version}/schemas/schemas/${replace(var.topic_name, ".", "/")}.avsc"
 }
 
 resource "google_pubsub_schema" "main" {
-  name       = "${var.topic_name}-${var.schema_version}"
+  name       = "${replace(var.topic_name, ".", "-")}-${var.schema_version}"
   type       = "AVRO"
   definition = data.http.schema_definition.body
 }
 
 resource "google_pubsub_topic" "main" {
-  name = "${var.topic_name}-${var.schema_version}"
+  name  = "${replace(var.topic_name, ".", "-")}-${var.schema_version}"
 
   depends_on = [google_pubsub_schema.main]
   schema_settings {
-    schema   = "projects/${var.project_id}/schemas/${var.topic_name}-${var.schema_version}"
+    schema   = "projects/${var.project_id}/schemas/${replace(var.topic_name, ".", "-")}-${var.schema_version}"
     encoding = "JSON"
   }
 }
