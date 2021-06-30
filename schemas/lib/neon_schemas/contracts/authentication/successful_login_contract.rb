189e0dbed110ba8f91b4d563db5ffbeda39e1ee9
nick shook <nick@shook.family>
2021-06-29 21:12:15 -0700

    fix error

--- schemas/lib/neon_schemas/contracts/authentication/successful_login_contract.rb
+++ schemas/lib/neon_schemas/contracts/authentication/successful_login_contract.rb
@@ -1,20 +1,16 @@
-require "dry-validation"
+require 'dry-validation'
 
-module NeonSchemas
-  module Contracts
-    module Authentication
-      class SuccessfulLoginContract < Dry::Validation::Contract
-        json do
-          required(:email).filled(:string)
-          required(:sub).value(:string)
-        end
+module NeonSchemas::Contracts::Authentication
+  class SuccessfulLoginContract < Dry::Validation::Contract
+    json do
+      required(:email).filled(:string)
+      required(:sub).value(:string)
+    end
 
-        rule(:email) do
-          return if EMAIL_REGEX.match?(value)
+    rule(:email) do
+      return if EMAIL_REGEX.match?(value)
 
-          key.failure("has invalid format")
-        end
-      end
+      key.failure('has invalid format')
     end
   end
 end
--- schemas/lib/neon_schemas/contracts/outbound_emails/welcome_email_contract.rb
+++ schemas/lib/neon_schemas/contracts/outbound_emails/welcome_email_contract.rb
@@ -1,22 +1,18 @@
-require "dry-validation"
+require 'dry-validation'
 
-module NeonSchemas
-  module Contracts
-    module OutboundEmails
-      class WelcomeEmailContract < Dry::Validation::Contract
-        json do
-          required(:email).filled(:string)
-          required(:sub).value(:string)
-        end
+module NeonSchemas::Contracts::OutboundEmails
+  class WelcomeEmailContract < Dry::Validation::Contract
+    json do
+      required(:email).filled(:string)
+      required(:sub).value(:string)
+    end
 
-        rule(:email) do
-          email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    rule(:email) do
+      email_regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-          return if email_regex.match?(value)
+      return if email_regex.match?(value)
 
-          key.failure("has invalid format")
-        end
-      end
+      key.failure('has invalid format')
     end
   end
 end
