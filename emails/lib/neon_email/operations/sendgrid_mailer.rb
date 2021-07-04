54508f827afd8f89a5f270c6a71fd7b057ead25b
nick <nick@shook.family>
2021-07-04 08:56:46 -0700

    add class specs for emails (#2660)

--- emails/lib/neon_email/operations/sendgrid_mailer.rb
+++ emails/lib/neon_email/operations/sendgrid_mailer.rb
@@ -4,16 +4,27 @@ require "neon_secrets"
 module NeonEmail
   module Operations
     class SendgridMailer < NeonOperations::Operation
-      def call
-        from_email = SendGrid::Email.new(email: "support@neonlaw.com")
-        to_email = SendGrid::Email.new(email: to)
-        sendgrid_content = SendGrid::Content.new(
-          type: "text/plain",
-          value: content
-        )
-        SendGrid::Mail.new(from_email, subject, to_email, sendgrid_content)
+      def initialize(
+        input:,
+        schema_contract: NeonSchemas::Contracts::OutboundEmails::SendgridMailContract,
+        mailer_operation: NeonEmail::Operations::SendgridMailer
+      )
+        @input = input
+        @schema_contract = schema_contract
+      end
 
-        Success
+      def call
+        validate_input.bind do |validated_input|
+          from_email = SendGrid::Email.new(email: "support@neonlaw.com")
+          to_email = SendGrid::Email.new(email: validated_input[:to])
+          sendgrid_content = SendGrid::Content.new(
+            type: "text/plain",
+            value: content
+          )
+          SendGrid::Mail.new(from_email, subject, to_email, sendgrid_content)
+
+          Success
+        end
       rescue
         Failure("there was a Sendgrid error")
       end
