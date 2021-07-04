ab4c1b3216fb8bdbcd93210936ecaeef4fe306ec
nick <nick@shook.family>
2021-07-03 22:56:54 -0700

    need version bump of neon_operations (#2652)
    To continue with our spec we need a version bump of neon_operations

--- emails/lib/neon_email/operations/greeting_mailer.rb
+++ emails/lib/neon_email/operations/greeting_mailer.rb
@@ -3,10 +3,6 @@ require_relative "./sendgrid_mailer"
 module NeonEmail
   module Operations
     class GreetingMailer < NeonOperations::Operation
-      def self.call(input:)
-        new(input: input).call
-      end
-
       def initialize(
         input:,
         schema_contract: NeonSchemas::Contracts::OutboundEmails::WelcomeEmailContract.new,
@@ -17,7 +13,7 @@ module NeonEmail
       end
 
       def call
-        validate_input.bind do |validated_input|
+        Success(validate_input).bind do |validated_input|
           mailer_operation.call(
             from: "support@neonlaw.com",
             to: validated_input.fetch("to"),
@@ -25,14 +21,6 @@ module NeonEmail
             content: "Welcome, please email us if you have any questions."
           )
         end
-      end
-
-      private
-
-      attr_reader :input, :schema_contract
-
-      def validate_input
-        schema_contract.call(input)
       end
     end
   end
