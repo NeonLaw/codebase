4a655ae5f5c07f085605ef1762e88603929bde60
nick shook <nick@shook.family>
2021-07-09 08:53:51 -0700

    add slack functions for 1-at-a-time

--- infrastructure/gcp/modules/functions/main.tf
+++ infrastructure/gcp/modules/functions/main.tf
@@ -1,4 +1,4 @@
-resource "google_cloudfunctions_function" "main" {
+resource "google_cloudfunctions_function" "welcome_email" {
   name        = "welcome-email-${var.color}"
   description = "Send Welcome Email"
   runtime     = "ruby27"
@@ -16,6 +16,32 @@ resource "google_cloudfunctions_function" "main" {
       retry = false
     }
   }
+
+  environment_variables = {
+    NEON_ENV = var.environment
+  }
+}
+
+resource "google_cloudfunctions_function" "send_slack_message" {
+  name        = "welcome-email-${var.color}"
+  description = "Send Slack Message"
+  runtime     = "ruby27"
+
+  available_memory_mb   = 256
+  source_archive_bucket = var.source_archive_bucket
+  source_archive_object = var.email_source_archive_object
+  entry_point           = "slack.send_message"
+
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = "projects/${var.project_id}/topics/slack-send-message-${var.schema_version}"
+
+    failure_policy {
+      retry = false
+    }
+  }
+
+  max_instances = 1
 
   environment_variables = {
     NEON_ENV = var.environment
