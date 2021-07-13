5bba1c707991f304565854fd4dfd116c8705e839
Abraham <anubra266@gmail.com>
2021-07-13 21:46:29 +0100

    chore(eslint): no linting on vercel

--- web/next.config.js
+++ web/next.config.js
@@ -2,26 +2,24 @@ const withPlugins = require('next-compose-plugins');
 const withNextIntl = require('@moxy/next-intl/plugin');
 const withMDX = require('@next/mdx');
 
-module.exports = withPlugins([
-  withMDX(),
-  withNextIntl(),
-], {
+module.exports = withPlugins([withMDX(), withNextIntl()], {
   env: {
-    environment: process.env.NODE_ENV
+    environment: process.env.NODE_ENV,
   },
+  eslint: { ignoreDuringBuilds: true },
   pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
   webpack(config) {
     config.module.rules.push({
       test: /\.mp3$/,
       use: {
-        loader: 'file-loader'
+        loader: 'file-loader',
       },
     });
 
     config.module.rules.push({
       test: /\.pdf$/,
       use: {
-        loader: 'file-loader'
+        loader: 'file-loader',
       },
     });
     return config;
