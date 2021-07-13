07cf02d58aa4202d5c53d64fb96586e1b19fd9e8
nick <nick@shook.family>
2021-07-13 00:20:47 -0700

    Add Slack test with specs to write message (#2709)
    This is tested using the Doppler environment `slack` which has our team's access token to write to the `devops` channel.

--- slack/spec/spec_helper.rb
+++ slack/spec/spec_helper.rb
@@ -5,10 +5,11 @@ require "faker"
 require "functions_framework/testing"
 require "functions_framework"
 require "base64"
+require "dry/monads"
 
 RSpec.configure do |config|
   config.include FunctionsFramework::Testing
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.include Dry::Monads[:result]
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
