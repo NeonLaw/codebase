45ed05d2ddd4b6dd83acfb1adc2c4e7aa80fd160
nisarhassan12 <syednisarhassan12@gmail.com>
2021-07-14 23:19:48 +0500

    migrate base styles to use chakra ui only.

--- web/src/styles/globalStyles.ts
+++ web/src/styles/globalStyles.ts
@@ -1,13 +1,48 @@
-import { colors, gutters, theme } from './neonLaw';
+import { colors, gutters, sizes, theme } from './neonLaw';
+
+import { colors as DydColors } from './deleteYourData';
 
 export const globalStyles = {
   styles: {
-    global: ({ colorMode }) => ({
+    global: ({ colorMode }) => ({    
       '.desktop-half-mobile-full-card-link': {
         'img': {
           maxHeight: '350px',
           objectFit: 'cover'
         }
+      },
+      '.full-bleed': {
+        left: '50%',
+        marginLeft: '-50vw',
+        marginRight: '-50vw',
+        position: 'relative',
+        right: '50%',
+        width: '100vw',
+      },
+      '.heading__underlined': {
+        '&--centered': {
+          '&::after': {
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }
+        },
+        '&--cyan': {
+          '&::after': {
+            background: colors.primaryColor400,
+          }
+        },
+        '&--orange': {
+          '&::after': {
+            background: DydColors.primary,
+          }
+        },
+        '&::after': {
+          content: '""',
+          display: 'block',
+          height: '2px',
+          margin: `${gutters.xSmallOne} 0`,
+          width: '8rem',
+        },
       },
       '.mdx-ul': {
         'li': {
@@ -16,9 +51,67 @@ export const globalStyles = {
           }
         }
       },
+      '.nav-content': {
+        '&-desktop': {
+          '@media (min-width: 1201px)': {
+            display: 'flex',
+          },
+          display: 'none',
+        },
+        '&-mobile': {
+          '@media (min-width: 1201px)': {
+            display: 'none !important',
+          }
+        }
+      },
+      '.nav-link': {
+        '&--active': {
+          '&::after': {
+            background: colors.primaryColor400,
+          },
+          color: colors.primaryColor400,
+        }
+      },
+      '.outline-bordered': {
+        border: '2px solid transparent',
+      },
       '.portal-layout': {
         'h2, .h2': {
           fontSize: '2.5rem'
+        }
+      },
+      '.row': {
+        margin: '0 auto',
+        maxWidth: 'var(--grid-max-width)',
+        width: '90%',
+      },
+      '.visually-hidden': {
+        height: '1px',
+        left: '-100000rem',
+        overflow: 'hidden',
+        position: 'absolute',
+        top: 'auto',
+        width: '1px',
+      },
+      '.wrapper': {
+        '&--centered': {
+          margin: ' 0 auto',
+          maxWidth: sizes.textContainerMedium,
+          padding: gutters.small,
+        }
+      },
+      ':root': {
+        '--grid-max-width': '1240px',
+        '--lightBlue': '#63b3ed',
+        '--outline': '2px solid var(--lightBlue)',
+        '--outline-transparent': '2px solid transparent',
+      },
+      '@keyframes pulse': {
+        '0%': {
+          transform: 'scale(0.9)',
+        },
+        '100%': {
+          transform: 'scale(1.02)',
         }
       },
       'a': {
@@ -31,6 +124,33 @@ export const globalStyles = {
         cursor: 'pointer',
         textDecoration: 'underline',
         transition: 'all .2s',
+      },
+      /* eslint-disable-next-line max-len */
+      'a:focus, a:active, button::-moz-focus-inner, input::-moz-focus-inner, select::-moz-focus-inner': {
+        border: 0,
+      },
+      'body': {
+        fontSize: theme.fontSizes.md
+      },
+      'body.user-is-tabbing': {
+        '.breadcrumb:focus': {
+          boxShadow: 'none',
+        },
+        '.outline-bordered:focus': {
+          border: 'var(--outline)',
+          outline: 'none',
+        },
+      },
+      'body:not(.user-is-tabbing)': {
+        'button:focus, input:focus, select:focus, textarea:focus, a:focus': {
+          outline: 'none',
+        }
+      },
+      'button:focus, input:focus, select:focus, textarea:focus, a:focus': {
+        outline: 'var(--outline)',
+      },
+      'code': {
+        color: `${colors.text.darkLight} !important`,
       },
       'h1': {
         fontSize: theme.fontSizes['xl2']
@@ -55,8 +175,20 @@ export const globalStyles = {
         fontSize: '2rem',
         marginBottom: gutters.xSmall,
       },
+      'html': {
+        overflowX: 'hidden'
+      },
+      'img': {
+        objectFit: 'contain'
+      },
       'li': {
         listStyle: 'none',
+      },
+      section: {
+        '@media (max-width: 600px)': {
+          padding: `${gutters.largeTwo} 0`,
+        },
+        padding: `${gutters.largeOne} 0`,
       },
       'ul': {
         'li': {
